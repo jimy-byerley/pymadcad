5150c041a64657b7a2994484e87990eb90314fd9
jimy-byerley <jimy.byerley@gmail.com>
2020-02-17 11:03:08 +0100

    reworked Arc and created Circle

--- primitives.py
+++ primitives.py
@@ -1,5 +1,5 @@
 from math import sqrt
-from mathutils import vec3, normalize, anglebt, project, cos, sin, pi, length, cross, vec2, mat2, determinant, inverse, dot, atan, acos
+from mathutils import vec3, mat3, normalize, anglebt, project, cos, sin, pi, length, cross, vec2, mat2, determinant, inverse, dot, atan, acos
 import settings
 
 class Primitive(object):
@@ -10,6 +10,8 @@ class Primitive(object):
 		return None
 	def annotate(self, key, value):	
 		self.annotations[key] = value
+	def transform(self, trans):
+		return self
 	
 	def mesh(self, resolution=None):
 		return NotImplemented
@@ -52,49 +54,65 @@ class Arc(object):
 		self.a, self.b, self.c = a,b,c
 	
 	def center(self):
-		n = normalize(cross(self.a-self.b, self.c-self.b))
-		# (pi,ri) est une mediatrice
-		p1, p2 = (self.b+self.a)/2, (self.b+self.c)/2
-		r1, r2 = cross(n,self.b-self.a), cross(n,self.c-self.b)
-		return axisintersection((p1,r1), (p2,r2))
-	
-	def center(self):
 		a,c = self.a-self.b, self.c-self.b
 		l1,l2 = length(a),length(c)
-		t = dot(a,c)
-		d = (l1/l2-t) * 1/sin(acos(t))
+		t = dot(a,c)/(l1*l2)
+		h = 0.5 * (l1/l2-t) * 1/sqrt(1-t**2)
 		n = normalize(cross(self.a-self.b, self.c-self.b))
-		return (self.c+self.b)/2 + cross(n, self.b-self.c) * d/2
-		
+		return (self.c+self.b)/2 + cross(n, self.b-self.c) * h
 	
-	def mesh(self):
+	def mesh(self, resolution=None):
 		center = self.center()
 		angle = anglebt(self.a-center, self.c-center)
 		r = length(self.a-center)
-		div = settings.curve_resolution(angle, 2*angle*r)
+		div = settings.curve_resolution(angle, angle*r, resolution)
 		x = normalize(self.a-center)
 		y = self.b-center
 		y = normalize(y - project(y,x))
+		pts = [self.a]
+		for i in range(1,div):
+			a = angle * i/div
+			pts.append(x*r*cos(a) + y*r*sin(a) + center)
+		pts.append(self.c)
+		return pts, 'arc'
+
+
+def tangentellipsis(axis1, axis2, resolution=None):
+	''' axis directions doesn't need to be normalized nor oriented '''
+	x,y = -axis1[1], -axis2[1]
+	a,b,_ = inverse(mat3(x,y,cross(x,y))) * (axis2[0]-axis1[0])
+	a = -a
+	angleapprox = acos(dot(x,-y))
+	origin = axis1[0] - a*x
+	div = settings.curve_resolution(angleapprox*min(a,b), angleapprox, resolution)
+	pts = []
+	for i in range(div+1):
+		u = pi/2 * i/div
+		pts.append(x*a*(1-cos(u)) + y*b*(1-sin(u)) + origin)
+	return pts
+
+def dirbase(dir, align=vec3(1,0,0)):
+	x = normalize(align - project(align, self.axis[1]))
+	if length(x) < NUMPREC:		
+		align = vec3(align[1],-align[0],align[2])
+		x = normalize(align - project(align, self.axis[1]))
+	y = normalize(cross(self.axis[1], x))
+	return x,y,dir
+
+class Circle:
+	def __init__(self, axis, radius, alignment=vec3(1,0,0)):
+		self.axis, self.radius = axis, radius
+		self.alignment = alignment
+	def mesh(self, resolution=None):
+		center = self.axis[0]
+		x,y,z = dirbase(self.axis[1], self.alignment)
+		angle = 2*pi
+		div = settings.curve_resolution(angle*radius, angle, resolution)
 		pts = []
 		for i in range(div+1):
 			a = angle * i/div
 			pts.append(x*r*cos(a) + y*r*sin(a) + center)
 		return pts, 'arc'
-
-def axisintersection(a1, a2):
-	p1,r1 = a1
-	p2,r2 = a2
-	# pseudo inverse du systeme: choix de la reduction a opérer
-	m1 = mat2(r1[0], r2[0], r1[1], r2[1])
-	m2 = mat2(r1[0], r2[0], r1[2], r2[2])
-	m3 = mat2(r1[1], r2[1], r1[2], r2[2])
-	dm1 = abs(determinant(m1))
-	dm2 = abs(determinant(m2))
-	dm3 = abs(determinant(m3))
-	if dm1 >= dm2 and dm1 >= dm3:		x1,x2 = inverse(m1)*vec2(p1-p2)
-	elif dm2 >= dm3:					x1,x2 = inverse(m2)*vec2(p1[0]-p2[0], p1[2]-p2[2])
-	else:								x1,x2 = inverse(m3)*vec2(p1[1]-p2[1], p1[2]-p2[2])
-	return p1 - x1*r1
 
 
 if __name__ == '__main__':
