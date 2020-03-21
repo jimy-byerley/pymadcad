4bb6196fee89f33e2066583130a343467da629b6
jimy-byerley <jimy.byerley@gmail.com>
2020-03-21 21:45:37 +0100

    completed with all existing functionnalities

--- __init__.py
+++ __init__.py
@@ -6,14 +6,23 @@
 	
 	data types
 	----------
-	
+		TODO
 '''
 version = '0'
 
-from . import mathutils, mesh, generation, cut
+from . import mathutils, mesh, generation, cut, primitives, constraints, kinematic, io
+from . import view, text
 
 from .mathutils import *
 from .mesh import Mesh, Wire, Line, MeshError, web
-from .cut import bevel, beveltgt, planeoffsets
+from .cut import chamfer, bevel, beveltgt, planeoffsets
 from .generation import flatsurface, junction, extrans, extrusion, revolution, saddle, tube
+from .primitives import Axis, Segment, Arc, Circle, TangentEllipsis
+from .constraints import SolveError, Tangent, Distance, Angle, Parallel, Radius, Projected, PointOn, solve
+from .kinematic import Solid, InSolid, Pivot, Plane
+from .selection import select
+from .io import read, write
+
+from .text import Text
+from .view import Scene
 
