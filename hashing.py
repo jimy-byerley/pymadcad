5ee60487004ccd374f819833e4779aad465a7422
jimy byerley <jimy.byerley@gmail.com>
2020-04-09 12:01:54 +0200

    fixed rasterization issues with lines, and derivative limits with triangles

--- hashing.py
+++ hashing.py
@@ -1,8 +1,8 @@
-from copy import deepcopy
-from mathutils import vec3, ivec3, noproject, norminf, ceil, normalize, length, dot, glm, cross
+from mathutils import vec3, ivec3, noproject, norminf, normalize, dot, glm, cross, length, NUMPREC
 from mesh import Web
 from functools import reduce
-from math import nan, inf, floor, ceil
+from math import floor, ceil, sqrt, inf
+
 
 class PositionMap:
 	''' Holds objects assoiated with their location
@@ -31,9 +31,10 @@ class PositionMap:
 		elif isinstance(space, tuple) and len(space) == 2:
 			# permutation of coordinates to get the direction the closer to Z
 			n = glm.abs(space[1]-space[0])
-			if   n[1] >= n[0] and n[1] >= n[2]:	order = (2,0,1)
-			elif n[0] >= n[1] and n[0] >= n[2]:	order = (1,2,0)
-			else:								order = (0,1,2)
+			if max(n) < NUMPREC*max((max(p) for p in space)):	return
+			if   n[1] >= n[0] and n[1] >= n[2]:	order,reorder = (2,0,1),(1,2,0)
+			elif n[0] >= n[1] and n[0] >= n[2]:	order,reorder = (1,2,0),(2,0,1)
+			else:								order,reorder = (0,1,2),(0,1,2)
 			space = [ vec3(p[order[0]], p[order[1]], p[order[2]])	for p in space]
 			
 			# prepare variables
@@ -50,7 +51,7 @@ class PositionMap:
 			else:			zmin,zmax = space[1][2], space[0][2]
 			zmin -= zmin%cell
 			for i in range(max(1,ceil((zmax-zmin)/cell))):
-				z = zmin+cell*i+cell2
+				z = zmin + cell*i + cell2
 				
 				# y selection
 				if dy > 0:	ymin,ymax = fy(z-cell2), fy(z+cell2)
@@ -67,15 +68,16 @@ class PositionMap:
 						x = xmin + k*cell + cell2
 						
 						p = (x,y,z)
-						yield tuple([floor(p[i]/cell)		for i in order])
+						yield tuple([floor(p[i]/cell)		for i in reorder])
 		
 		# triangle
 		elif isinstance(space, tuple) and len(space) == 3:
 			# permutation of coordinates to get the normal the closer to Z
 			n = glm.abs(cross(space[1]-space[0], space[2]-space[0]))
-			if   n[1] >= n[0] and n[1] >= n[2]:	order = (2,0,1)
-			elif n[0] >= n[1] and n[0] >= n[2]:	order = (1,2,0)
-			else:								order = (0,1,2)
+			if max(n) < NUMPREC*max((max(p) for p in space)):	return
+			if   n[1] >= n[0] and n[1] >= n[2]:	order,reorder = (2,0,1),(1,2,0)
+			elif n[0] >= n[1] and n[0] >= n[2]:	order,reorder = (1,2,0),(2,0,1)
+			else:								order,reorder = (0,1,2),(0,1,2)
 			space = [ vec3(p[order[0]], p[order[1]], p[order[2]])	for p in space]
 			
 			# prepare variables
@@ -84,6 +86,7 @@ class PositionMap:
 			dx = -n[0]/n[2]
 			dy = -n[1]/n[2]
 			o = space[0]
+			fz = lambda x,y:	o[2] + dx*(x-o[0]) + dy*(y-o[1])
 			cell2 = cell/2
 			pmin = reduce(glm.min, space)
 			pmax = reduce(glm.max, space)
@@ -104,19 +107,19 @@ class PositionMap:
 						d = v[i][1]/(v[i][0] if v[i][0] else inf)
 						cand.append( space[i][1] + d * (x-cell2-space[i][0]) )
 						cand.append( space[i][1] + d * (x+cell2-space[i][0]) )
-				ymin,ymax = min(cand), max(cand)
+				ymin,ymax = max(pmin[1],min(cand)), min(pmax[1],max(cand))
 				ymin -= ymin%cell
+				#print(cand)
 				for j in range(max(1,ceil((ymax-ymin)/cell))):
 					y = ymin + cell*j + cell2
 				
 					# z selection
-					f = lambda x,y:	o[2] + dx*(x-o[0]) + dy*(y-o[1])
 					cand = []
-					cand.append( f(x-cell2, y-cell2) )
-					cand.append( f(x+cell2, y-cell2) )
-					cand.append( f(x-cell2, y+cell2) )
-					cand.append( f(x+cell2, y+cell2) )
-					zmin,zmax = min(cand), max(cand)
+					cand.append( fz(x-cell2, y-cell2) )
+					cand.append( fz(x+cell2, y-cell2) )
+					cand.append( fz(x-cell2, y+cell2) )
+					cand.append( fz(x+cell2, y+cell2) )
+					zmin,zmax = max(pmin[2],min(cand)), min(pmax[2],max(cand))
 					zmin -= zmin%cell
 					for k in range(max(1,ceil((zmax-zmin)/cell))):
 						z = zmin + cell*k + cell2
@@ -124,7 +127,7 @@ class PositionMap:
 						# remove box from corners that goes out of the area
 						p = (x,y,z)
 						if pmin[0]<p[0] and pmin[1]<p[1] and pmin[2]<p[2] and p[0]<pmax[0] and p[1]<pmax[1] and p[2]<pmax[2]:
-							yield tuple([floor(p[order[i]]/cell)	for i in range(3)])
+							yield tuple([floor(p[i]/cell)	for i in reorder])
 		
 		else:
 			raise TypeError("PositionMap only supports keys of type:  points, segments, triangles")
@@ -174,6 +177,11 @@ class PositionMap:
 			web += Web([base*(p+k)  for p in self._display.points], self._display.edges, groups=[k])
 		return web.display(scene)
 
+def meshcellsize(mesh):
+	''' returns a good cell size to index primitives of a mesh with a PositionMap '''
+	# the number of key for triangles is propotionate to the surface
+	# points are placed on this surface, so sqrt(len(pts)) is a good hint for the point density on the surface
+	return length(mesh.box().width) / sqrt(len(mesh.points))
 
 
 class PointSet:
@@ -238,11 +246,12 @@ class PointSet:
 
 if __name__ == '__main__':
 	from mesh import Mesh
+	from nprint import nprint
 	
 	triangles = Mesh([
 						vec3(1,1,0), vec3(3,1,0), vec3(1,3,0),
-						vec3(0,1,1), vec3(0,3,1), vec3(0,1,3),
-						vec3(1,0,1), vec3(3,0,1), vec3(1,0,3),
+						vec3(0,1,1), vec3(0,3,1), vec3(0,1,4),
+						vec3(1,0,1), vec3(3,0,1), vec3(1,0,4),
 						vec3(1,2,5), vec3(-3,0,4), vec3(4,-2,7),
 						],
 					[(0,1,2), (3,4,5), (6,7,8), (9,10,11)],
@@ -250,20 +259,23 @@ if __name__ == '__main__':
 	lines = Web([
 					vec3(5,0,0), vec3(5,0,4),
 					vec3(0,5,0), vec3(0,6,4),
-					vec3(-6,-8,9), vec3(-2,-5,2), 
+					#vec3(-6,-8,9), vec3(-2,-5,2), 
+					vec3(-6,-7,2), vec3(-4,-5,9),
 					],
 				[(0,1),(2,3),(4,5)],
 				)
-	m = PositionMap(1, [
+	m = PositionMap(3.7, [
 		(triangles.facepoints(0), 'x'),
 		(triangles.facepoints(1), 'y'),
 		(triangles.facepoints(2), 'z'),
 		(triangles.facepoints(3), 'truc'),
 		((vec3(5,0,0), vec3(5,0,4)), 'a'),
 		((vec3(0,5,0), vec3(0,6,4)), 'b'),
-		((vec3(-6,-8,9), vec3(-2,-5,2)), 'c'),
+		((vec3(-6,-7,2), vec3(-4,-5,9)), 'c'),
 		])
-	m.options['color'] = (0.7, 0.9, 1)
+	
+	nprint(m.dict)
+	print(set( m.get((vec3(8,0,0),vec3(0,8,0),vec3(-2,-2,8))) ))
 	
 	from mathutils import Box,fvec3
 	import view
@@ -271,6 +283,7 @@ if __name__ == '__main__':
 	from PyQt5.QtWidgets import QApplication
 	app = QApplication(sys.argv)
 	scn = view.Scene()
+	m.options['color'] = (0.7, 0.9, 1)
 	scn.add(m)
 	scn.add(triangles)
 	scn.add(lines)
