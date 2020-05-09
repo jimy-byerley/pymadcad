16422e4602049abfb7e6be6459109de3b6a4318c
jimy-byerley <jimy.byerley@gmail.com>
2020-05-09 21:56:56 +0200

    added contributors' compiled version of intersect_triangle

--- madcad/core.pyx
+++ madcad/core.pyx
@@ -14,7 +14,6 @@ cdef:
 		'''
 		m = fmod(l,r)
 		if m < 0:	m += r
-		assert m >= 0
 		return m
 	
 	struct cvec3:
@@ -88,6 +87,8 @@ def rasterize_segment(spaceo, double cell):
 	cdef long pk[3]
 	cdef cvec3 space[2]
 	
+	if cell <= 0:	raise ValueError('cell must be strictly positive')
+	
 	space = [glm2c(spaceo[0]), glm2c(spaceo[1])]
 	rasterization = []
 	
@@ -149,6 +150,8 @@ def rasterize_triangle(spaceo, double cell):
 	cdef cvec3 pmin, pmax
 	cdef double xmin,xmax, ymin,ymax, zmin,zmax
 	cdef cvec3 space[3]
+	
+	if cell <= 0:	raise ValueError('cell must be strictly positive')
 	
 	space = [glm2c(spaceo[0]), glm2c(spaceo[1]), glm2c(spaceo[2])]
 	rasterization = []
@@ -225,7 +228,6 @@ def rasterize_triangle(spaceo, double cell):
 					pk = [key(x,cell), key(y,cell), key(z,cell)]
 					rasterization.append(( pk[reorder[0]], pk[reorder[1]], pk[reorder[2]] ))
 	return rasterization
-
 
 """
 cdef struct cvec3 :
@@ -417,12 +419,362 @@ def intersect_triangles(f0, f1, double precision):
 	#sA=max(max_v(abs(xA))[0], max_v(abs(yA))[0])
 	cdef double sA
 	cdef size_t iA1, iA2
-	sA=max(max_dv(abs_dv3(xA).x, 3, &iA1), max_dv(abs_dv3(yA).x, 3, &iA2))
+	sA = max(max_dv(vabs(xA), 3, &iA1), max_dv(vabs(yA), 3, &iA2))
 	
 	#sB=max(max_v(abs(xB))[0], max_v(abs(yB))[0])
 	cdef double sB
 	cdef size_t iB1, iB2
-	sB=max(max_dv(abs_dv3(xB).x, 3, &iB1), max_dv(abs_dv3(yB).x, 3, &iB2))
+	sB = max(max_dv(vabs(xB), 3, &iB1), max_dv(vabs(yB), 3, &iB2))
+	
+	# identify signs of yA and yB
+	cdef int sYA[3]
+	cdef int sYB[3]
+	for i in range(3):
+		if fabs(varr(&yA)[i])/sA < precision:
+			sYA[i]=0
+			varr(&yA)[i]=0.
+		else : 
+			sYA[i]=sign(varr(&yA)[i])
+		if fabs(varr(&yB)[i])/sB < precision:
+			sYB[i]=0
+			varr(&yB)[i]=0.
+		else : 
+			sYB[i]=sign(varr(&yB)[i])
+
+	# check if triangles have no intersections with line D
+	if fabs(sum_iv(sYA, 3))==3 or fabs(sum_iv(sYB,3))==3 : 
+		#print("plans intersects but no edges intersection")
+		return None
+
+	# we know that triangles do intersect the line D
+	# edges of intersection on A and B with the convention : edge i of face X connects fX[i] and fX[(i+1)%3] 
+	cdef int eIA[2]
+	cdef int eIB[2]
+	cdef int neIA=0
+	cdef int neIB=0
+	for i in range(3):
+		if sYA[i]*sYA[(i+1)%3]<0 and fabs(sYA[i])+fabs(sYA[(i+1)%3])>0 : 
+			eIA[neIA]=i
+			neIA=neIA+1
+		elif sYA[i]==0 :
+			if sYA[(i+1)%3] != 0 : 
+				eIA[neIA]=i
+				neIA=neIA+1
+			else :
+				eIA[neIA]=(i-1)%3
+				neIA=neIA+1
+		if sYB[i]*sYB[(i+1)%3]<0 and fabs(sYB[i])+fabs(sYB[(i+1)%3])>0 : 
+			eIB[neIB]=i
+			neIB=neIB+1
+		elif sYB[i]==0 :
+			if sYB[(i+1)%3] != 0 : 
+				eIB[neIB]=i
+				neIB=neIB+1
+			else :
+				eIB[neIB]=(i-1)%3
+				neIB=neIB+1
+	if neIA==1 :
+		eIA[1]=eIA[0]
+	if neIB==1 :
+		eIB[1]=eIB[0]
+
+	# intersections coordinates onto line D
+	cdef double xIA[2]
+	cdef double xIB[2]
+	for i in range(2):
+		xIA[i]=(varr(&yA)[(eIA[i]+1)%3]*varr(&xA)[eIA[i]]-varr(&yA)[eIA[i]]*varr(&xA)[(eIA[i]+1)%3])/(varr(&yA)[(eIA[i]+1)%3]-varr(&yA)[eIA[i]])
+		xIB[i]=(varr(&yB)[(eIB[i]+1)%3]*varr(&xB)[eIB[i]]-varr(&yB)[eIB[i]]*varr(&xB)[(eIB[i]+1)%3])/(varr(&yB)[(eIB[i]+1)%3]-varr(&yB)[eIB[i]])
+
+	# intervals of intersections
+	cdef double xPIA0, xMUA0, xPIB0, xMIB0
+	cdef int xPIA1, xMIA1, xPIB1, xMIB1
+	xPIA0=max_dv(xIA, 2, &xPIA1)
+	xMIA0=min_dv(xIA, 2, &xMIA1)
+	xPIB0=max_dv(xIB, 2, &xPIB1)
+	xMIB0=min_dv(xIB, 2, &xMIB1)
+
+	# one intersection at the border of the intervals
+	if cfabs(xPIA0-xMIB0)/(0.5*(sB+sA))<precision_d : 
+		# edge of max from A matches min of B
+		#print("edges intersection PA-MB")
+		return (0, eIA[xPIA1], c2glm(sum_mult_dv3(pA1, d, xIA[xPIA1]).x)), \
+			(1, eIB[xMIB1], c2glm(sum_mult_dv3(pA1, d, xIB[xMIB1]).x))
+	if cfabs(xPIB0-xMIA0)/(0.5*(sB+sA))<precision_d : 
+		# edge of max from B matches min of A
+		#print("edges intersection PB-MA")
+		return (0, eIA[xMIA1], c2glm(sum_mult_dv3(pA1, d, xIA[xMIA1]).x)), \
+			(1, eIB[xPIB1], c2glm(sum_mult_dv3(pA1, d, xIB[xPIB1]).x))
+		
+	# no intersection
+	if xPIB0-precision_d<xMIA0 or xPIA0-precision_d<xMIB0:
+		#print("plans intersects but no edges intersection")
+		return None
+	
+	# one interval is included into the other one
+	if xMIB0-precision_d<xMIA0 and xPIA0-precision_d<xPIB0:
+		# edges of A cross face B
+		#print("edges of A intersect B")
+		return (0, eIA[xMIA1], c2glm(sum_mult_dv3(pA1, d, xIA[xMIA1]).x)), \
+			(0, eIA[xPIA1], c2glm(sum_mult_dv3(pA1, d, xIA[xPIA1]).x))
+	if xMIA0-precision_d<xMIB0 and xPIB0-precision_d<xPIA0:
+		# edges of B cross face A
+		#print("edges of B intersect A")
+		return (1, eIB[xMIB1], c2glm(sum_mult_dv3(pA1, d, xIB[xMIB1]).x)), \
+			(1, eIB[xPIB1], c2glm(sum_mult_dv3(pA1, d, xIB[xPIB1]).x))
+
+	# intervals cross each other
+	if xMIB0>xMIA0-precision_d and xPIA0-precision_d<xPIB0:
+		# M edge of B crosses face A and P edge of A crosses face B
+		#print("M edge of B intersects A and P edge of A intersects B")
+		return (0, eIA[xPIA1], c2glm(sum_mult_dv3(pA1, d, xIA[xPIA1]).x)), \
+			(1, eIB[xMIB1], c2glm(sum_mult_dv3(pA1, d, xIB[xMIB1]).x))
+	if xMIA0>xMIB0-precision_d and xPIB0-precision_d<xPIA0:
+		# M edge of A crosses face B and P edge of B crosses face A
+		#print("M edge of A intersects B and P edge of B intersects A")
+		return (0, eIA[xMIA1], c2glm(sum_mult_dv3(pA1, d, xIA[xMIA1]).x)), \
+			(1, eIB[xPIB1], c2glm(sum_mult_dv3(pA1, d, xIB[xPIB1]).x))
+
+	print("intersect_triangles: unexpected case : ", fA, fB)
+	return None
+"""
+import glm 
+
+ctypedef struct dv3 :
+	double x[3]
+
+# routines toF simplify intersections
+cdef double max_dv(double *x, int n, int *im):
+	cdef double m=x[0]
+	cdef int i
+	im[0]=0
+	
+	for i in range(1,n):
+		if x[i]>m:
+			m=x[i]
+			im[0]=i
+	return m
+
+cdef double min_dv(double *x, int n, int*im):
+	cdef double m=x[0]
+	cdef int i
+	im[0]=0
+	for i in range(1,n):
+		if x[i]<m:
+			m=x[i]
+			im[0]=i
+	return m
+
+cdef double max_d(double d1, double d2):
+	if (d1<d2) :
+		return d2
+	return d1
+
+cdef double min_d(double d1, double d2):
+	if (d1>d2) :
+		return d2
+	return d1
+
+cdef int sign_d(double d) :
+	if (d > 0) :
+		return 1
+	if (d < 0) :
+		return -1
+	return 0
+
+
+def conv_v(r,x):
+	if isinstance(r, glm.fvec3): 
+		return glm.fvec3(x)
+	if isinstance(r, glm.dvec3): 
+		return glm.dvec3(x)
+	return x
+
+
+cdef dv3 minus_dv3(dv3 v1, dv3 v2) :
+	cdef dv3 r
+	r.x[0]=v1.x[0]-v2.x[0]
+	r.x[1]=v1.x[1]-v2.x[1]
+	r.x[2]=v1.x[2]-v2.x[2]
+	return r
+
+
+cdef dv3 plus_dv3(dv3 v1, dv3 v2) :
+	cdef dv3 r
+	r.x[0]=v1.x[0]+v2.x[0]
+	r.x[1]=v1.x[1]+v2.x[1]
+	r.x[2]=v1.x[2]+v2.x[2]
+	return r
+
+cdef dv3 cross_dv3(dv3 v1, dv3 v2) :
+	cdef dv3 r
+	r.x[0]=v1.x[1]*v2.x[2]-v1.x[2]*v2.x[1]
+	r.x[1]=v1.x[2]*v2.x[0]-v1.x[0]*v2.x[2]
+	r.x[2]=v1.x[0]*v2.x[1]-v1.x[1]*v2.x[0]
+	return r
+
+cdef double dot_dv3(dv3 v1, dv3 v2) :
+	return v1.x[0]*v2.x[0]+v1.x[1]*v2.x[1]+v1.x[2]*v2.x[2]
+
+from libc.math cimport sqrt as csqrt
+from libc.math cimport abs as cabs
+
+
+cdef double length_dv3(dv3 v1) :
+	return csqrt(v1.x[0]*v1.x[0]+v1.x[1]*v1.x[1]+v1.x[2]*v1.x[2])
+
+cdef dv3 normalize_dv3(dv3 v1) :
+	cdef dv3 r
+	cdef double n = length_dv3(v1)
+	r.x[0]=v1.x[0]/n
+	r.x[1]=v1.x[1]/n
+	r.x[2]=v1.x[2]/n
+	return r
+	
+cdef dv3 mult_dv3(dv3 v1, double d) :
+	cdef dv3 r
+	r.x[0]=v1.x[0]*d
+	r.x[1]=v1.x[1]*d
+	r.x[2]=v1.x[2]*d
+	return r
+	
+from libc.math cimport fabs as cfabs    
+cdef dv3 abs_dv3(dv3 v) : 
+	cdef dv3 r
+	r.x[0]=cfabs(v.x[0])
+	r.x[1]=cfabs(v.x[1])
+	r.x[2]=cfabs(v.x[2])
+	return r
+	
+cdef dv3 sum_mult_dv3(dv3 v1, dv3 v2, double d) : 
+	cdef dv3 r
+	r.x[0]=v1.x[0]+v2.x[0]*d
+	r.x[1]=v1.x[1]+v2.x[1]*d
+	r.x[2]=v1.x[2]+v2.x[2]*d
+	return r
+
+cdef int sum_iv(int *v, int n) :
+	cdef int i
+	cdef int r=0
+	for i in range(n) : 
+		r=r+v[i]
+	return r
+		
+# main intersection routine
+def intersect_triangles(f0, f1, precision=1e-7):
+	''' Intersects 2 triangles and outputs intersections vertices
+
+		f0 = first face (tuple of 3 vertices given in clock wise orientation. vertices are glm.vec3 or glm.dvec3)
+		f1 = second face
+
+		output = None if no intersection
+				2 intersection vertices given as : 
+				((fi, ej, xj), (fk, em, xm)) where
+					fi, fk = face id
+					ej, em = edges id on faces
+					xj, xm = intersection point (same precision as input vertices) 
+					
+		restrictions : vertices on faces must be spatially different. identical vertices on triangles are not managed
+	'''
+	# computation in double precision
+	cdef dv3 fA[3]
+	cdef dv3 fB[3]
+	cdef int i
+	for i in range(3) :
+		fA[i].x[0] = f0[i][0]
+		fA[i].x[1] = f0[i][1]
+		fA[i].x[2] = f0[i][2]
+		fB[i].x[0] = f1[i][0]
+		fB[i].x[1] = f1[i][1]
+		fB[i].x[2] = f1[i][2]
+	cdef double precision_d = precision
+
+
+	# gets the normal to the fisrt face
+	#A1A2=fA[1]-fA[0]
+	cdef dv3 A1A2=minus_dv3(fA[1], fA[0])
+	#A1A3=fA[2]-fA[0]
+	cdef dv3 A1A3=minus_dv3(fA[2], fA[0])
+	#nA1=cross(A1A2, A1A3)
+	cdef dv3 nA1=cross_dv3(A1A2, A1A3)
+	#nA=normalize(nA1)
+	cdef dv3 nA=normalize_dv3(nA1)
+	
+	# gets the normal to the second face
+	#B1B2=fB[1]-fB[0]
+	cdef dv3 B1B2=minus_dv3(fB[1], fB[0])
+	#B1B3=fB[2]-fB[0]
+	cdef dv3 B1B3=minus_dv3(fB[2], fB[0])
+	#nB1=cross(B1B2, B1B3)
+	cdef dv3 nB1=cross_dv3(B1B2, B1B3)
+	#nB=normalize(nB1)
+	cdef dv3 nB=normalize_dv3(nB1)
+	
+	# gets the direction of the intersection between the plan containing fA and the one containing fB 
+	#d1=cross(nA, nB)
+	cdef dv3 d1=cross_dv3(nA, nB)
+	#ld1=length(d1)
+	cdef double ld1=length_dv3(d1)
+	if ld1 < precision_d :
+		#print("coplanar or parallel faces")
+		return None
+		
+	# normalize d1
+	#d=d1/ld1
+	cdef dv3 d=mult_dv3(d1, 1.0/ld1)
+
+	# projection direction on to d from fA and fB
+	#tA=cross(nA, d)
+	cdef dv3 tA=cross_dv3(nA, d)
+	#tB=cross(nB, d)
+	cdef dv3 tB=cross_dv3(nB, d)
+
+	# project fA summits onto d (in pfA)
+	# xA being the coordinates of fA onto d
+	# pA1=fA[0]-dot(fA[0]-fB[0], nB)/dot(tA,nB)*tA
+	cdef dv3 pA1=minus_dv3(fA[0], mult_dv3(tA, dot_dv3(minus_dv3(fA[0],fB[0]), nB)/dot_dv3(tA,nB)))
+	
+	#xA=dvec3(0., dot(A1A2, d), dot(A1A3, d))
+	cdef dv3 xA
+	xA.x[0]=0.
+	xA.x[1]=dot_dv3(A1A2, d)
+	xA.x[2]=dot_dv3(A1A3, d)
+	#pfA=(pA1, pA1+xA[1]*d, pA1+xA[2]*d)
+	cdef dv3 pfA[3]
+	pfA[0]=pA1
+	for i in range(1,3):
+		pfA[i]=sum_mult_dv3(pA1, d, xA.x[i])
+	
+	# project fB summits onto d
+	#xB=dvec3(dot(fB[0]-fA[0], d), dot(fB[1]-fA[0], d), dot(fB[2]-fA[0], d))
+	cdef dv3 fAfB[3]
+	cdef dv3 xB
+	for i in range(3):
+		fAfB[i]=minus_dv3(fB[i], fA[0])
+		xB.x[i]=dot_dv3(fAfB[i], d)
+	#pfB=(pA1+xB[0]*d, pA1+xB[1]*d, pA1+xB[2]*d)
+	cdef dv3 pfB[3]
+	for i in range(3):
+		pfB[i]=sum_mult_dv3(pA1, d, xB.x[i])
+	
+	# project fA and fB summits on transversal direction tA tB
+	cdef dv3 yA
+	cdef dv3 yB
+	for i in range(3):
+		#yA[i]=dot((fA[i]-pfA[i]), tA)
+		yA.x[i]=dot_dv3(minus_dv3(fA[i], pfA[i]), tA)
+		#yB[i]=dot((fB[i]-pfB[i]), tB)
+		yB.x[i]=dot_dv3(minus_dv3(fB[i], pfB[i]), tB)
+
+	# dimensioning for fA and fB
+	#sA=max(max_v(abs(xA))[0], max_v(abs(yA))[0])
+	cdef double sA
+	cdef int iA1, iA2
+	sA=max_d(max_dv(abs_dv3(xA).x, 3, &iA1), max_dv(abs_dv3(yA).x, 3, &iA2))
+	
+	#sB=max(max_v(abs(xB))[0], max_v(abs(yB))[0])
+	cdef double sB
+	cdef int iB1, iB2
+	sB=max_d(max_dv(abs_dv3(xB).x, 3, &iB1), max_dv(abs_dv3(yB).x, 3, &iB2))
 	
 	# identify signs of yA and yB
 	cdef int sYA[3]
@@ -437,7 +789,7 @@ def intersect_triangles(f0, f1, double precision):
 			sYB[i]=0
 			yB.x[i]=0.
 		else : 
-			sYB[i]=sign(yB.x[i])
+			sYB[i]=sign_d(yB.x[i])
 
 	# check if triangles have no intersections with line D
 	if cabs(sum_iv(sYA, 3))==3 or cabs(sum_iv(sYB,3))==3 : 
@@ -495,13 +847,13 @@ def intersect_triangles(f0, f1, double precision):
 	if cfabs(xPIA0-xMIB0)/(0.5*(sB+sA))<precision_d : 
 		# edge of max from A matches min of B
 		#print("edges intersection PA-MB")
-		return (0, eIA[xPIA1], c2glm(sum_mult_dv3(pA1, d, xIA[xPIA1]).x)), \
-			(1, eIB[xMIB1], c2glm(sum_mult_dv3(pA1, d, xIB[xMIB1]).x))
+		return (0, eIA[xPIA1], conv_v(f0[0], sum_mult_dv3(pA1, d, xIA[xPIA1]).x)), \
+			(1, eIB[xMIB1], conv_v(f0[0], sum_mult_dv3(pA1, d, xIB[xMIB1]).x))
 	if cfabs(xPIB0-xMIA0)/(0.5*(sB+sA))<precision_d : 
 		# edge of max from B matches min of A
 		#print("edges intersection PB-MA")
-		return (0, eIA[xMIA1], c2glm(sum_mult_dv3(pA1, d, xIA[xMIA1]).x)), \
-			(1, eIB[xPIB1], c2glm(sum_mult_dv3(pA1, d, xIB[xPIB1]).x))
+		return (0, eIA[xMIA1], conv_v(f0[0], sum_mult_dv3(pA1, d, xIA[xMIA1]).x)), \
+			(1, eIB[xPIB1], conv_v(f0[0], sum_mult_dv3(pA1, d, xIB[xPIB1]).x))
 		
 	# no intersection
 	if xPIB0-precision_d<xMIA0 or xPIA0-precision_d<xMIB0:
@@ -512,27 +864,25 @@ def intersect_triangles(f0, f1, double precision):
 	if xMIB0-precision_d<xMIA0 and xPIA0-precision_d<xPIB0:
 		# edges of A cross face B
 		#print("edges of A intersect B")
-		return (0, eIA[xMIA1], c2glm(sum_mult_dv3(pA1, d, xIA[xMIA1]).x)), \
-			(0, eIA[xPIA1], c2glm(sum_mult_dv3(pA1, d, xIA[xPIA1]).x))
+		return (0, eIA[xMIA1], conv_v(f0[0], sum_mult_dv3(pA1, d, xIA[xMIA1]).x)), \
+			(0, eIA[xPIA1], conv_v(f0[0], sum_mult_dv3(pA1, d, xIA[xPIA1]).x))
 	if xMIA0-precision_d<xMIB0 and xPIB0-precision_d<xPIA0:
 		# edges of B cross face A
 		#print("edges of B intersect A")
-		return (1, eIB[xMIB1], c2glm(sum_mult_dv3(pA1, d, xIB[xMIB1]).x)), \
-			(1, eIB[xPIB1], c2glm(sum_mult_dv3(pA1, d, xIB[xPIB1]).x))
+		return (1, eIB[xMIB1], conv_v(f0[0], sum_mult_dv3(pA1, d, xIB[xMIB1]).x)), \
+			(1, eIB[xPIB1], conv_v(f0[0], sum_mult_dv3(pA1, d, xIB[xPIB1]).x))
 
 	# intervals cross each other
 	if xMIB0>xMIA0-precision_d and xPIA0-precision_d<xPIB0:
 		# M edge of B crosses face A and P edge of A crosses face B
 		#print("M edge of B intersects A and P edge of A intersects B")
-		return (0, eIA[xPIA1], c2glm(sum_mult_dv3(pA1, d, xIA[xPIA1]).x)), \
-			(1, eIB[xMIB1], c2glm(sum_mult_dv3(pA1, d, xIB[xMIB1]).x))
+		return (0, eIA[xPIA1], conv_v(f0[0], sum_mult_dv3(pA1, d, xIA[xPIA1]).x)), \
+			(1, eIB[xMIB1], conv_v(f0[0], sum_mult_dv3(pA1, d, xIB[xMIB1]).x))
 	if xMIA0>xMIB0-precision_d and xPIB0-precision_d<xPIA0:
 		# M edge of A crosses face B and P edge of B crosses face A
 		#print("M edge of A intersects B and P edge of B intersects A")
-		return (0, eIA[xMIA1], c2glm(sum_mult_dv3(pA1, d, xIA[xMIA1]).x)), \
-			(1, eIB[xPIB1], c2glm(sum_mult_dv3(pA1, d, xIB[xPIB1]).x))
+		return (0, eIA[xMIA1], conv_v(f0[0], sum_mult_dv3(pA1, d, xIA[xMIA1]).x)), \
+			(1, eIB[xPIB1], conv_v(f0[0], sum_mult_dv3(pA1, d, xIB[xPIB1]).x))
 
 	print("intersect_triangles: unexpected case : ", fA, fB)
 	return None
-
-"""
