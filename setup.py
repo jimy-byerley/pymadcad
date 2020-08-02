62a12d11be8a6a42d1e6ee196348e36341941259
jimy-byerley <jmy.byerley@gmail.com>
2020-08-02 22:11:49 +0200

    try to get rid of the cython dependency

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-from setuptools import setup, find_packages
-from Cython.Build import cythonize
+from setuptools import setup, find_packages, Extension
+#from Cython.Build import cythonize
 
 setup(
 	# package declaration
 	name='pymadcad',
 	version='0.1',
-	install_requires=['pyglm>=1.2', 'moderngl>=5.6', 'numpy>=1.1', 'scipy>=0.17', 'PIL>=5.4', 'PyQt5>=5'],
+	python_requires='>=3.5',
+	install_requires=['pyglm>=1.2', 'moderngl>=5.6', 'numpy>=1.1', 'scipy>=0.17', 'PyQt5>=5', 'Pillow>=5.4'],
 	extras_require={
 		'PLY': ['plyfile>=0.7'],
 		'STL': ['numpy-stl>=2'],
@@ -15,10 +16,24 @@ setup(
 		},
 	# source declaration
 	packages=find_packages(),
-	ext_modules=cythonize(['madcad/core.pyx'], annotate=True),
+	#ext_modules=cythonize(['madcad/core.pyx'], annotate=True),
+	ext_modules=[Extension('madcad.core', ['madcad/core.c'])],
+	package_data={
+		'madcad': ['shaders/*.frag', 'shaders/*.vert', 'textures/*.png', '*.py', '*.pyx', '*.c'],
+		'': ['COPYING', 'COPYING.LESSER', 'README'],
+		},
 	
 	# metadata for pypi
-	author='jimy byerley',
+	author='Yves Dejonghe',
 	author_email='jimy.byerley@gmail.com',
-	description="core library of the madcad project, it's time to throw parametric softwares out !",
+	description="Simple yet powerful CAD (Computer Aided Design) library, written with Python",
+	long_description=open('README.md').read(),
+	long_description_content_type='text/markdown',
+	license='GNU LGPL v3',
+	url='https://github.com/jimy-byerley/pymadcad',
+	keywords='CAD 3D parametric mesh kinematic solid solver part design',
+	classifiers=[
+		'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+		'Topic :: Scientific/Engineering',
+		],
 	)
