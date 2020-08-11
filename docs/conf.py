d9b84a259811701512c996715385e68335a31b5d
jimy-byerley <jmy.byerley@gmail.com>
2020-08-11 18:49:11 +0200

    removed path addition for inplace built project as it is incompatible with readthedoc online compilation

--- docs/conf.py
+++ docs/conf.py
@@ -12,10 +12,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-from os.path import *
-import sys
-sys.path.insert(0, abspath(dirname(__file__)+sep+'..'))
-
 
 # -- Project information -----------------------------------------------------
 
