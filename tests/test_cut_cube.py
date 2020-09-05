ad642b5f90b8d85367c55f09704b53e5e266a59f
jimy-byerley <jmy.byerley@gmail.com>
2020-09-05 11:40:25 +0200

    test/example fully operational

--- tests/test_cut_cube.py
+++ tests/test_cut_cube.py
@@ -6,8 +6,8 @@ from madcad import scaledir, normalize
 cube = brick(Box(center=vec3(0), width=vec3(2))) #.transform(scaledir(normalize(vec3(1)), 0.5))
 w = [(0,1),(1,2),(2,3),(0,3),(1,5),(0,4)]
 #multicut(cube, w, ('width', 0.3))
-chamfer(cube, w, ('width', 0.3))
-#bevel(cube, w, ('width', 0.3))
+#chamfer(cube, w, ('width', 0.3))
+bevel(cube, w, ('width', 0.3))
 
 cube.mergeclose()
 cube.check()
