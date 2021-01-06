2832e88bcce9573164259a30f9eec1161a8fc790
jimy byerley <jimy.byerley@gmail.com>
2021-01-06 11:27:06 +0100

    added note_leading, note_label, note_distance, note_distance_planes, note_angle_edge, note_angle_planes

--- madcad/scheme.py
+++ madcad/scheme.py
@@ -5,10 +5,11 @@ from collections import namedtuple
 from .mathutils import *
 from .rendering import Display
 from .common import ressourcedir
-from .mesh import Container, Mesh, Web, Wire, web
-from .rendering import Displayable
+from .mesh import Container, Mesh, Web, Wire, web, wire
+from .rendering import Displayable, writeproperty
 from . import generation as gt
-from . import text
+from .primitives import *
+from . import text as txt
 from . import settings
 
 SVertex = namedtuple('SVertex', ['space', 'pos', 'normal', 'color', 'layer', 'track', 'flags'])
@@ -129,6 +130,10 @@ class Scheme:
 			
 			self.components = [(space,scene.display(obj))	for space,obj in sch.components]
 			
+			
+			self.nidents = max(v[5] for v in sch.vertices)+1
+			self.box = boundingbox(fvec3(v[1]) for v in sch.vertices if self.spacegens[v[0]] is world)
+			
 			# prepare the buffer of vertices
 			vertices = np.empty(len(sch.vertices), 'u1, 3f4, 3f4, 4u1, f4, u2, u1')
 			for i,v in enumerate(sch.vertices):
@@ -136,8 +141,7 @@ class Scheme:
 					*v[:3],
 					u8vec4(v[3]*255), 
 					*v[4:]
-					)
-			self.nidents = max(v[5] for v in sch.vertices)+1
+					)			
 			self.vb_vertices = ctx.buffer(vertices)
 			verticesdef = [(self.vb_vertices, 'u1 3f4 3f4 4f1 f4 u2 u1', 
 								'space', 
@@ -303,10 +307,11 @@ class note_leading_display(Display):
 		self.origin = fvec3(origin)
 		self.offset = fvec3(offset)
 		self.comment = comment
+		self._world = fmat4(1)
 		
 		def build(side):
-			color = fvec4(settings.display['annotation_color'],0.7)
-			sch = Scheme(color=color)
+			color = settings.display['annotation_color']
+			sch = Scheme(color=fvec4(color,0.7))
 			sch.add([self.origin, self.origin+self.offset], shader='line', space=world)
 			x,y,z = dirbase(normalize(vec3(self.offset)))
 			sch.add(
@@ -319,7 +324,7 @@ class note_leading_display(Display):
 				)
 			sch.set(space=halo_screen(self.origin+self.offset))
 			sch.add([vec3(0), vec3(side*(20 - 9*0.4), 0, 0)], shader='line')
-			sch.add(text.Text(vec3(side*20, 0, 0), self.comment, align=('right' if side>0 else 'left', 0.5), color=color, size=9))
+			sch.add(txt.Text(vec3(side*20, 0, 0), self.comment, align=('right' if side>0 else 'left', 0.5), color=fvec4(color,1), size=9))
 			
 			return scene.display(sch)
 		self.disp = build(1), build(-1)
@@ -338,43 +343,241 @@ class note_leading_display(Display):
 	
 	def stack(self, scene):
 		return ((), 'screen', 2, self.render), ((), 'ident', 2, self.identify)
+		
+	@writeproperty
+	def world(self, value):
+		for disp in self.disp:	disp.world = value
 
 
-def note_leading(origin, direction, comment):
-	return Displayable(note_leading_display, origin, direction, comment)
+def mesh_placement(mesh):
+	if isinstance(mesh, Mesh):
+		# group center normal
+		center = mesh.barycenter()
+		f = min(mesh.faces,
+				key=lambda f:  length2(center - sum(mesh.facepoints(f))/3)
+				)
+		normal = mesh.facenormal(f)
+		pos = sum(mesh.facepoints(f)) / 3
+	
+	elif isinstance(mesh, Web):
+		center = mesh.barycenter()
+		e = min(mesh.edges,
+				key=lambda e:  length2(center - (mesh.points[a]+mesh.points[b])/2)
+				)
+		normal = dirbase(normalize(mesh.points[e[0]]-mesh.points[e[1]]))[0]
+		pos = mix(mesh.points[e[0]], mesh.points[e[1]], 0.5)
+		
+	elif isaxis(mesh):
+		pos, normal = mesh
+	
+	elif isinstance(mesh, vec3):
+		normal = vec3(0)
+		pos = mesh
+	
+	else:
+		raise TypeError('unable to place note on a {}'.format(type(mesh)))
+	return pos, normal
 
-def note_group(part, group, comment):
-	# group center
-	center = sum(	sum(part.facepoints(f))
-					for f,t in zip(part.faces, part.tracks)
-					if t == group
-					) / (3*len(part.faces))
-	# group center normal
-	f = min(part.faces,
-			key=lambda f:  length2(center - sum(part.facepoints(f))/3)
-			)
-	size = length(boundingbox(part).width)
-	return note_leading(sum(part.facepoints(f))/3, 0.3*size*part.facenormal(f), comment)
+def note_leading(placement, offset=None, text='here'):
+	origin, normal = mesh_placement(placement)
+	if not offset:
+		offset = 0.2 * length(boundingbox(placement).width) * normal
+	return Displayable(note_leading_display, origin, offset, text)
 
-def note_floating(position, comment, *args, **kwargs):
-	return text.Text(position, comment, *args, **kwargs, color=settings.display['annotation_color'], size=9)
 
-def note_distance(a, b, offset, d=None, tol=None):
+def note_floating(position, text, *args, **kwargs):
+	return txt.Text(position, text, *args, **kwargs, color=settings.display['annotation_color'], size=9)
+
+#def note_distance(a, b, offset=0, project=None, d=None, tol=None, text=None):
+def note_distance(a, b, offset=vec3(0), d=None, tol=None, text=None):
+	# get text to display
 	if not d:	d = distance(a,b)
+	if not text:
+		if isinstance(tol,str): text = '{d:.4g}  {tol}'
+		elif tol:               text = '{d:.4g}  ± {tol}'
+		else:                   text = '{d:.4g}'
+	text = text.format(d=d, tol=tol)
+	color = settings.display['annotation_color']
+	# convert input vectors
 	a = fvec3(a)
 	b = fvec3(b)
-	offset = fvec3(offset)
-	ao = a+offset
-	bo = b+offset
+	o = fvec3(offset)
+	if o == fvec3(0):	ao, bo = a, b
+	else:
+		ao = a + o * (1 - 0.5*dot(o,a-b)/length2(o))
+		bo = b + o * (1 - 0.5*dot(o,b-a)/length2(o))
+	# create scheme
 	sch = Scheme()
-	sch.set(shader='line', layer=-1e-4, color=fvec4(settings.display['annotation_color'],0.3))
+	sch.set(shader='line', layer=1e-4, color=fvec4(color,0.3))
 	sch.add([a, ao])
 	sch.add([b, bo])
-	sch.set(layer=1e-4, color=fvec4(settings.display['annotation_color'],0.7))
+	sch.set(layer=-1e-4, color=fvec4(color,0.7))
 	sch.add([ao, bo])
-	sch.add(text.Text(mix(ao,bo,0.5), '{:.6g}  ± {}'.format(d, tol) if tol else '{:.6g}'.format(d), align=('center','center'), size=9, color=fvec4(settings.display['annotation_color'],1)))
+	sch.add(txt.Text(
+				mix(ao,bo,0.5), 
+				text, 
+				align=('center','center'), 
+				size=9, 
+				color=fvec4(color,1)))
 	sch.set(shader='fill')
-	n,_,x = dirbase(normalize(vec3(a-b)))
-	sch.add(gt.revolution(2*pi, (vec3(0),x), web([vec3(0), 1.5*n+6*x]), resolution=('div',8)), space=scale_screen(ao))
-	sch.add(gt.revolution(2*pi, (vec3(0),x), web([vec3(0), 1.5*n-6*x]), resolution=('div',8)), space=scale_screen(bo))
+	n,_,x = dirbase(normalize(vec3(ao-bo)))
+	sch.add(gt.revolution(
+				2*pi, 
+				(vec3(0),x), 
+				web([vec3(0), 1.5*n+6*x]), 
+				resolution=('div',8)), 
+			space=scale_screen(ao))
+	sch.add(gt.revolution(
+				2*pi, 
+				(vec3(0),x), 
+				web([vec3(0), 1.5*n-6*x]), 
+				resolution=('div',8)), 
+			space=scale_screen(bo))
 	return sch
+	
+def note_distance_planes(s0, s1, offset=None, d=None, tol=None, text=None):
+	n0 = s0.facenormal(0)
+	n1 = s1.facenormal(1)
+	if length2(cross(n0,n1)) > NUMPREC**2:
+		raise ValueError('surfaces are not parallel')
+	
+	p0 = mesh_placement(s0)[0]
+	p1 = mesh_placement(s1)[0]
+	if not offset:
+		offset = noproject(boundingbox(s0,s1).width, n0) * 0.6
+	shift = project(p1 - p0, normalize(cross(n0,offset))) * 0.5
+	return note_distance(p0+shift, p1-shift, offset, d, tol, text)
+	
+def note_distance_set(s0, s1, offset, d=None, tol=None, text=None):
+	indev
+		
+
+def note_angle(a0, a1, offset=0, d=None, tol=None, text=None, unit='deg'):
+	o0, d0 = a0
+	o1, d1 = a1
+	z = normalize(cross(d0,d1))
+	x0 = cross(d0,z)
+	x1 = cross(d1,z)
+	shift = project(o1-o0, z) * 0.5
+	o0 += shift
+	o1 -= shift
+	# get text to display
+	if not d:	d = anglebt(d0, d1)
+	if unit == 'deg':
+		d *= 180/pi
+		unit = '°'
+	if not text:
+		if isinstance(tol,str): text = '{d:.4g}{unit}  {tol}'
+		elif tol:               text = '{d:.4g}{unit}  ± {tol}'
+		else:                   text = '{d:.4g}{unit}'
+	text = text.format(d=d, tol=tol, unit=unit)
+	color = settings.display['annotation_color']
+	# arc center
+	if o1 == o0 or d1 == d0:	center = o0
+	else:						center = o0 + unproject(project(o1-o0, x1), d0)
+	radius = mix(distance(o0,center), distance(o0,center), 0.5) + offset
+	# arc extremities
+	p0 = center+radius*d0
+	p1 = center+radius*d1
+	sch = Scheme()
+	sch.set(shader='line', layer=1e-4, color=fvec4(color,0.3))
+	sch.add([p0, o0])
+	sch.add([p1, o1])
+	arc = ArcCentered((center,z), p0, p1, ('rad',0.05)).mesh()
+	sch.add(arc, color=fvec4(color,0.7))
+	sch.set(layer=-1e-4)
+	sch.add(txt.Text(
+				arc[len(arc)//2], 
+				text, 
+				align=('center','center'), 
+				size=9, 
+				color=fvec4(color,1)))
+	sch.set(shader='fill')
+	sch.add(gt.revolution(
+				2*pi, 
+				(vec3(0),x0), 
+				web([vec3(0), 1.5*d0+6*x0]), 
+				resolution=('div',8)), 
+			space=scale_screen(fvec3(p0)))
+	sch.add(gt.revolution(
+				2*pi, 
+				(vec3(0),x1), 
+				web([vec3(0), 1.5*d1-6*x1]), 
+				resolution=('div',8)), 
+			space=scale_screen(fvec3(p1)))
+	return sch
+	
+def _mesh_direction(mesh):
+	if isinstance(mesh, Mesh):	return mesh.facenormal(0)
+	elif isinstance(mesh, Web):	return mesh.edgedirection(0)
+	elif isinstance(mesh, Wire):	return mesh[1]-mesh[0]
+	elif isaxis(mesh):	return mesh[1]
+	else:
+		raise TypeError('only Mesh and Web are supported')
+	
+def note_angle_planes(s0, s1, offset=0, d=None, tol=None, text=None, unit='deg'):
+	''' place an angle quotation between 2 meshes considered to be plane (surface) or straight (curve) '''
+	d0, d1 = _mesh_direction(s0), _mesh_direction(s1)
+	z = cross(d0, d1)
+	if isinstance(s0, Mesh) or isaxis(s0):	d0 = normalize(cross(d0,z))
+	if isinstance(s1, Mesh) or isaxis(s1):	d1 = normalize(cross(z,d1))
+	return note_angle(
+				(mesh_placement(s0)[0], d0), 
+				(mesh_placement(s1)[0], d1), 
+				offset, d, tol, text, unit)
+				
+def note_angle_edge(part, edge, offset=0, d=None, tol=None, text=None, unit='deg'):
+	f0 = None
+	f1 = None
+	for face in part.faces:
+		for i in range(3):
+			if face[i-1] == edge[0] and face[i] == edge[1]:	
+				f0 = face
+			elif face[i-1] == edge[1] and face[i] == edge[0]:
+				f1 = face
+	if not f0 or not f1:
+		raise ValueError("edge {} doesn't exist or is not between 2 faces".format(f0))
+	d0 = part.facenormal(f0)
+	d1 = part.facenormal(f1)
+	z = normalize(cross(d0,d1))
+	o = mix(part.points[edge[0]], part.points[edge[1]], 0.5)
+	if not offset:	offset = 0.2 * length(boundingbox(part).width)
+	return note_angle(
+			(o, cross(z,d0)),
+			(o, cross(d1,z)),
+			offset, d, tol, text, unit)
+	
+def note_absciss(axis, pts):
+	indev
+	
+def note_label(placement, offset=None, text='!', style='rect'):
+	p, normal = mesh_placement(placement)
+	if not offset:	
+		size = length(boundingbox(placement).width)
+		offset = 0.2 * length(boundingbox(placement).width) * normal
+	color = settings.display['annotation_color']
+	x,_,z = dirbase(normalize(offset))
+	sch = Scheme()
+	sch.set(color=fvec4(color,0.7))
+	sch.add(gt.revolution(
+				2*pi,
+				(vec3(0),z),
+				web([3*x, -5*z]),
+				resolution=('div',8),
+				),
+			space=scale_screen(fvec3(p)), shader='fill')
+	sch.add([p, p+offset], space=world, shader='line', layer=2e-4)
+	r = 5
+	if style == 'circle':	outline = Circle((vec3(0),vec3(0,0,1)), r)
+	elif style == 'rect':	outline = [vec3(r,r,0), vec3(-r,r,0), vec3(-r,-r,0), vec3(r,-r,0), vec3(r,r,0)]
+	else:
+		raise ValueError("style must be 'rect' or 'circle'")
+	sch.set(space=halo_screen(fvec3(p+offset)))
+	sch.add(outline, shader='line', layer=0)
+	sch.add(gt.flatsurface(wire(outline)), color=fvec4(settings.display['background_color'],0), shader='fill', layer=1e-4)
+	sch.add(txt.Text(p+offset, text, align=('center','center'), size=10, color=color), space=world)
+	return sch
+	
+
+def note_iso(p, offset, type, text, refs=(), label=None):
+	indev
---
+++ tests/test_scheme.py
@@ -0,0 +1,28 @@
+from madcad import *
+from madcad.scheme import *
+
+part = extrusion(vec3(-1,-3,-5), flatsurface(wire(Circle((vec3(1), vec3(0,0,1)), 1))))
+cube = brick(center=vec3(-4,0,0), width=vec3(1))
+part.mergeclose()
+
+show([
+	part,
+	cube,
+	#note_leading(vec3(1), vec3(2), text="This is a note\n  with\n  several lines"),
+	note_floating(vec3(-2,2,-2), text="break all sharp edges\n\nsurfaces must be Ra <= 1.6 unless otherwise indicated"),
+	#note_distance(vec3(0,0,1), vec3(0,0,-5), vec3(0,-3,0), tol=0.05),
+	#note_angle((vec3(0),normalize(vec3(1,0,0))), (vec3(0),normalize(vec3(-1,2,0))), offset=2, d=4),
+	#note_label(vec3(2,1,1), vec3(0,0,1), 'A'),
+	
+	note_leading(part.group(0), text="ø5 ± 0.05"),
+	note_label(part.group(2), text='A'),
+	
+	#note_cyclinder(part.group(0), tol=0.01),
+	note_distance_planes(part.group(1), part.group(2), tol='H7'),
+	#note_distance_set(part.group(1), part.group(2)),
+	#note_distance_set(part.frontier(0,1), part.frontier(0,2)),
+	note_angle_edge(part, (49,50)),
+	#note_angle_edge(part.group(0), part.group(1)),
+	note_angle_planes(cube.group(0), cube.group(2), offset=-1),
+	#note_angle_planes((O,x), (O,y+x)),
+	])
