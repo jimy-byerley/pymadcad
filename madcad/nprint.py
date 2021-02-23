2cfe0afe4233ef308c717fe0a38bc3b216d5921e
jimy-byerley <jimy.byerley@gmail.com>
2021-02-23 18:08:31 +0100

    added nprint.deformat() to remove all possible linebreaks in a code

--- madcad/nprint.py
+++ madcad/nprint.py
@@ -1,4 +1,5 @@
 # author:	Jimy Byerley (jimy.byerley@gmail.com)
+# license:	GNU-LGPLv3
 
 """ Intends to replace the well known pprint, to format python objects on output
 	
@@ -10,17 +11,19 @@
 	                           pass repr(obj) to nprint or nformat
 	nformat()     split a text into indented lines
 	ncolor()      add color makers for terminals syntax highlighting
+	deformat()   remove formatting of all marker-enclosed text
 """
 
 from itertools import chain
-import os
+import os, io
 
 
-separators = {',', ';'}
-begin_marker = {'{', '(', '['}
-end_marker = {'}', ')', ']'}
-symbols = {':', '*', '/', '+', '-', '<', '>'}
-specials = {'\\'}
+separators = ',;'
+begin_marker = '{(['
+end_marker = '})]'
+symbols = ':*/+-<>'
+specials = '\\'
+spaces = ' \t\n'
 
 line_width = 100
 
@@ -57,17 +60,17 @@ def _next_terminator(text, seek):
 		elif text[i] in end_marker:	counter -= 1
 		if counter < 0:		break
 	return i
-'''
-def next_beginner(text, seek):
-	for i in range(seek, len(text)):
-		if text[i] in begin_marker:	break
-	return i
-'''
 	
+def _next_word(text, seek):
+	while seek < len(text) and text[seek] in spaces:
+		seek += 1
+	return seek
+
+
 def nformat(text, indent=0, width=None):
-	""" output text, splited into indented blocks, newlines are kept """
+	''' output text, splited into indented blocks, newlines are kept '''
 	if type(text) != str:	text = str(text)
-	out = ""
+	out = io.StringIO()
 	seek = 0
 	if not width:	width = line_width
 	
@@ -76,28 +79,51 @@ def nformat(text, indent=0, width=None):
 			term = _next_terminator(text, seek+1)
 			if term - seek > width or '\n' in text[seek:term]:	# develop enclosement if its length is over that width
 				indent += 1
-				out += text[seek]
+				out.write(text[seek])
 				if _next_separator(text, seek+1) < term: 	# no return if there is only one element in the enclosement
-					out += '\n'+'\t'*indent
+					out.write( '\n'+'\t'*indent )
+					seek = _next_word(text, seek+1)-1
 			else:
-				out += text[seek:term+1]
+				out.write(text[seek:term+1])
 				seek = term
 		elif text[seek] in separators:
-			out += text[seek] + '\n'+'\t'*indent
+			out.write(text[seek] + '\n'+'\t'*indent)
+			seek = _next_word(text, seek+1)-1
 		elif text[seek] in end_marker:
 			indent -= 1
-			out += text[seek]
-		elif out and out[-1] in {'\t', '\n'} and text[seek] in {' ', '\t', '\n'}:	# remove spaced near ligne switch
-		#elif text[seek] in {' ', '\t', '\n'}:
-			pass
+			out.write(text[seek])
 		else:
-			out += text[seek]
+			out.write(text[seek])
 		
 		seek += 1
 	
-	return out
+	return out.getvalue()
+	
+def deformat(text):
+	''' remove formatting from text, leving only indentation and line breaks for lines not ended by a continuation character (,-+*/) '''
+	seek = 1
+	out = io.StringIO()
+	out.write(text[:seek])
+	
+	while seek < len(text):
+		if text[seek] in spaces:
+			jump = _next_word(text, seek)
+			if text[seek-1] in ',+-*/':
+				out.write(' ')
+			elif text[seek-1] in begin_marker or jump < len(text) and text[jump] in end_marker:
+				pass
+			elif '\n' in text[seek:jump]:
+				out.write('\n')
+			else:
+				out.write(' ')
+			seek = jump
+		else:
+			out.write(text[seek])
+			seek += 1
+	return out.getvalue()
+		
 
-def _dichotomy_index(l, index, key=lambda x:x):
+def _bisect(l, index, key=lambda x:x):
 	start,end = 0, len(l)
 	while start < end:
 		mid = (start+end)//2
@@ -108,10 +134,10 @@ def _dichotomy_index(l, index, key=lambda x:x):
 	return start
 
 def ncolor(text):
-	""" generates a string with the content of the text passed, with terminal color makers to highlight some syntax elements """
-	# liste des colorations a appliquer
+	''' generates a string with the content of the text passed, with terminal color makers to highlight some syntax elements '''
+	# list of colors to apply
 	coloration = []
-	# recherche des symboles spéciaux
+	# look for special symbols
 	for symbol in chain(separators, begin_marker, end_marker, symbols, specials):
 		start = text.find(symbol, 0)
 		while start != -1:
@@ -127,17 +153,17 @@ def ncolor(text):
 			elif symbol in specials:	color = color_specials
 			
 			coloration.insert(
-				_dichotomy_index(coloration, start, lambda x:x[1]), 
+				_bisect(coloration, start, lambda x:x[1]), 
 				(color, start, end-1)
 				)
 			start = text.find(symbol, end)
 	
-	# recherche des noms CamelCase
+	# look for special worlds
 	it = iter(text)
 	i = 0
 	for c in it:
 		if c.isalnum():
-			# recherce des nombres (base quelconque)
+			# look for numbers
 			if c.isdigit():
 				for j,c in enumerate(it):
 					if not c.isalnum():	
@@ -145,11 +171,11 @@ def ncolor(text):
 						break
 				j += 1
 				coloration.insert(
-					_dichotomy_index(coloration, i, lambda x:x[1]), 
+					_bisect(coloration, i, lambda x:x[1]), 
 					(color_number, i, i+j)
 					)
 				i += j+1
-			# recherche des noms CamelCase
+			# look for CamelCase names
 			#if c.isupper():
 				#for j,c in enumerate(it):
 					#if not c.isalnum():	
@@ -157,7 +183,7 @@ def ncolor(text):
 						#break
 				#j += 1
 				#coloration.insert(
-					#_dichotomy_index(coloration, i, lambda x:x[1]), 
+					#_bisect(coloration, i, lambda x:x[1]), 
 					#(color_camel, i, i+j)
 					#)
 				#i += j+1
@@ -192,3 +218,23 @@ if __name__ == '__main__':
 	#m = np.matrix([[1000,2324,30],[40000,5000,6342342]])
 	#nprint(repr(m))
 	#nprint(m)
+	print(deformat('''
+	P2 = vec3(0.08182,1.184e-08,-0.09931)
+	P3 = vec3(-0.0431,1.258e-08,-0.1056)
+P4 = vec3(-0.1593,-1.199e-08,0.1006)
+line = [
+	Segment(P4, P0),
+	Segment(P0, P1 ),
+	ArcThrough(P1, vec3(0.09681,-1.713e-09,0.01437), P2
+		),
+	Segment(P2,P3),
+	ArcThrough(P3,vec3(-0.06933,-1.117e-09,0.009369),P4),
+	]
+axis = Axis(
+	vec3(-0.1952,	4.918e-08,	-0.4126),
+	vec3(1,	0,	0))
+	'''))
+
+	# hardcore test
+	#import ast
+	#nprint(ast.dump(ast.parse(open(__file__).read())))
