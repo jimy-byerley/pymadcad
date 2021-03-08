36bd66515db0c2dfdf184fd06d5b6106290180c8
jimy-byerley <jimy.byerley@gmail.com>
2021-03-08 18:54:15 +0100

    improved boolean edgecross check

--- tests/test_boolean_edgecross.py
+++ tests/test_boolean_edgecross.py
@@ -1,7 +1,32 @@
 from madcad import *
 
-m1 = brick(width=vec3(2))
-m2 = m1.transform(vec3(1, -0.5, 1))
+# 2 bricks intersecting
+b1 = brick(width=vec3(2))
+b2 = b1.transform(vec3(1, -0.5, 1))
 
-m3 = boolean.difference(m1, m2)
-show([m3], options={'display_wire':True})
+br = boolean.difference(b1, b2)
+br.check()
+assert br.isenvelope()
+
+# cylinders intersecting
+o = vec3(0)
+z = vec3(0,0,1)
+
+def cylinder(axis, radius, height):
+	return extrusion(
+		-height*axis[1],
+		flatsurface(wire(Circle(axis, radius, resolution=('rad', pi/16)))),
+		)
+
+cyl = difference(
+	cylinder((o,z), 2, 2),
+	cylinder((vec3(2,0,1),z), 1, 4),
+	)
+cyl.check()
+assert cyl.isenvelope()
+
+# display when everything happened fine
+show([
+	br, 
+	cyl.transform(vec3(4,0,0)),
+	], options={'display_wire':True})
