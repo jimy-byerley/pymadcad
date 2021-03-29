3ef149f212b87cbe81dc977df0ad51569649313f
jimy byerley <jimy.byerley@gmail.com>
2021-03-29 12:53:39 +0200

    added O,X,Y,Z definitions for end-user

--- madcad/mathutils.py
+++ madcad/mathutils.py
@@ -13,7 +13,7 @@ any = __builtins__['any']
 all = __builtins__['all']
 round = __builtins__['round']
 
-
+# alias definitions
 vec2 = dvec2
 mat2 = dmat2
 vec3 = dvec3
@@ -22,12 +22,19 @@ vec4 = dvec4
 mat4 = dmat4
 quat = dquat
 
-NUMPREC = 1e-13
+# numerical precision of floats used
+NUMPREC = 1e-13	# float64 here, so 14 decimals
+#NUMPREC = 1e-6	# float32 here, so 7 decimals, so 1e-6 when exponent is 1
 COMPREC = 1-NUMPREC
 
-# numerical precision of floats used (float32 here, so 7 decimals, so 1e-6 when exponent is 1)
-#NUMPREC = 1e-6
-#COMPREC = 1-NUMPREC
+
+
+# common base definition, for end user
+O = vec3(0,0,0)
+X = vec3(1,0,0)
+Y = vec3(0,1,0)
+Z = vec3(0,0,1)
+
 
 def isfinite(x):
 	''' return false if x contains a `inf` or a `nan` '''
