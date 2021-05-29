604fd39e330f3eebea771c30fb211fb330c52feb
jimy-byerley <jimy.byerley@gmail.com>
2021-05-29 15:43:45 +0200

    some temptatives to fix the issues of sphinx with mathjax

--- docs/conf.py
+++ docs/conf.py
@@ -28,14 +28,14 @@ needs_sphinx = '3.2'
 # sphinx extensions
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
+    #'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
     'recommonmark',
     'sphinx_rtd_theme',
 ]
 
 # use a more recent version of mathjax, that can render ASCIIMATH
-mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/startup.js"
+#mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/startup.js"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
