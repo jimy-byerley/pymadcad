67736407a8334e6ef79b4b791db8b5fcceb2ca77
jimy-byerley <jimy.byerley@gmail.com>
2022-06-15 00:02:25 +0200

    fixed not in-place operation

--- madcad/mathutils.py
+++ madcad/mathutils.py
@@ -117,7 +117,7 @@ def scaledir(dir, factor=None) -> mat3:
 	'''
 	if factor is None:
 		factor = length(dir)
-		dir /= factor
+		dir = dir / factor
 	return mat3(1) + (factor-1)*mat3(dir[0]*dir, dir[1]*dir, dir[2]*dir)
 	
 def rotatearound(angle, *args) -> mat4:
