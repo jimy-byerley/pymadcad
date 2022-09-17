973a1e12648cffdf106e6d072acaa25e388dd0d7
jimy-byerley <jimy.byerley@gmail.com>
2022-09-17 16:40:13 +0200

    rtd experiment

--- docs/conf.py
+++ docs/conf.py
@@ -59,6 +59,7 @@ primary_domain = 'py'
 html_logo = 'logo.png'
 html_favicon = "logo.ico"
 html_static_path = ['static']	# path to custom static files, such as images and stylesheets
+html4_writer = True
 
 html_theme = 'sphinx_rtd_theme'
 
