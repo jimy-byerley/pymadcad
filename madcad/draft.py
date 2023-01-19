26207c02a64ab7cddc7cb829264972da966d0b77
GlennWSo <sondellglenn@gmail.com>
2023-01-19 12:51:59 +0100

    added proper distance function and test

--- madcad/draft.py
+++ madcad/draft.py
@@ -4,41 +4,103 @@ import numpy as np
 from madcad.prelude import *
 import madcad as cad
 
+def offset_vector(n1: vec3, n2: vec3):
+    """
+    if edge1 and edge2 is offset a distance(t) along their normals
+    returns the intersection offset/t
+    """
+    n3 = normalize(n1+n2)
+    cos = dot(n1, n3)
+    scale = 1 / cos
+    return n3 * scale
 
-def draft_extrusion(mesh: Mesh, trans: vec3, angle: float) -> Mesh:
-    ex = cad.extrusion(trans, mesh)
-    ex.finish()
-    trans_faces = ex.group(2).faces
-    face_arr = np.array([face.to_tuple() for face in trans_faces])
-    trans_inds = np.unique(face_arr)
+def add_draft(extruded: Mesh, trans: vec3, angle: float, free=2):
+    """
+    Mutates an extruded Mesh to give at a draft angle.
+
+    # Args:
+    - extruded(Mesh): the mesh to give draft angle,
+    - trans(vec3): vector of extrusion used
+    - angle(float): degrees
+    """
+    moved = extruded.group(free).outlines()
+    edge_arr = np.array([edge.to_tuple() for edge in moved.edges])
+    moved_inds = np.unique(edge_arr)
+
+    trans_normal = normalize(trans)
+    vertex_normals = {}
+    for e in moved.edges:
+        edir = moved.edgedirection(e)
+        normal = normalize(cross(edir, trans_normal))
+        vertex_normals[e[0], 0] = normal
+        vertex_normals[e[1], 1] = normal
+
+
+    exl = np.linalg.norm(trans)
+    for i in moved_inds:
+        vector = offset_vector(vertex_normals[i, 0], vertex_normals[i, 1])
+        scale = np.tan(np.deg2rad(angle)) * exl
+        extruded.points[i] += vector*scale
     
-    tube = ex.group(0)
-    n_trans = normalize(trans)
-    trans_len = np.linalg.norm(trans)   
-
-    face_inds = [i for i, face in enumerate(tube.faces )if 2 >len(set(face) - set(trans_inds))]
-    tube_normals = tube.facenormals()
-    derp = []
-    for i in trans_inds:
-        faces = [tube.faces[face_i] for face_i in face_inds if i in tube.faces[face_i]]
-        normals = [tube_normals[face_i] for face_i in face_inds if i in tube.faces[face_i]]
-        c1 = cross(*normals)
-        c2 = cross(c1, normals[0])
-        c3 = cross(normals[1], c1)
-        mixed = (c2 + c3 + sum(normals)) / 2
-        projected = mixed - n_trans*dot(mixed, n_trans)
-        offset = projected*trans_len * np.sin(np.deg2rad(angle))
-        ex.points[i] += offset
 
 
+def draft_extrusion(base: Mesh, trans: vec3, angle: float) -> Mesh:
+    ex = cad.extrusion(trans, base)
+    add_draft(ex, trans, angle)
     return ex
+
+
+def triangle() -> Mesh:
+    points = tlist([
+        [0,0,0], 
+        [0,1,0],
+        [1,1,0],
+        ], vec3)
+    faces = [[0,1,2]]
+    return Mesh(points, faces)
+
+
+def angle_vectors(v1: vec3, v2: vec3, degrees=False) -> float:
+    n1 = normalize(v1)
+    n2 = normalize(v2)
+    angle = np.arccos(dot(n1, n2))
+    if not degrees:
+        return angle
+    return np.rad2deg(angle)
+
+def test_draft(base: Mesh, plot=False):
+    trans = vec3(0, 0, 4)
+    ex = cad.extrusion(trans, base) 
+
+    draft_angle = 5  
+    print(f"adding draft angle {draft_angle}")
+    add_draft(ex, trans, -draft_angle)
+    ex.finish()
+
+    # measure the resulting face angles
+    tran_n = normalize(trans)
+    angles = []
+    print("resulting face angles compared to extrusion axis")
+    for normal in ex.facenormals():
+        angle = angle_vectors(normal, tran_n, degrees=True)
+        print(angle)
+        angles.append(np.round(angle, decimals=3))
     
+    if plot:
+        show([ex])
+    assert set(angles) == {95.0, 180.0, 0.0 }
+
+
+if __name__ == "__main__":
+    print("testing drafting on extruded triangle")
+    tribase = triangle()
+    test_draft(tribase, True)
+
+    print("testing drafting on extruded square")
+    sq_base = cad.square(Axis(vec3(), Z), 1)
+    test_draft(sq_base, True)
+
+
     
 
-if __name__== "__main__":
-    m = cad.square(Axis((0,0,0), Z), 2)
-    print(m.points)
-    ex = draft_extrusion(m, -8*Z, 30)
-    for p in ex.group(2).points:
-        print(p)
-    show([ex])
+
