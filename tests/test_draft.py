5f924442c29dcbc42402022cd03b3b8ef757757c
GlennWSo <sondellglenn@gmail.com>
2023-01-25 15:28:34 +0100

    clean up testing

--- tests/test_draft.py
+++ tests/test_draft.py
@@ -10,7 +10,8 @@ import madcad as cad
 
 from madcad.draft import draft_by_slice, draft_extrusion, draft_by_axis
 
-PLOT_DEFUALT = "--trace" in sys.argv or __name__ == "__main__"
+
+PLOT_DEFUALT = "--trace" in sys.argv or __name__ == "__main__" or "-v" in sys.argv
 
 
 def make_bases() -> List:
@@ -31,28 +32,46 @@ def make_bases() -> List:
 	return bases
 
 
-def make_extrusions() -> List[Tuple[Mesh, type]]:
-	bases = make_bases()
-	trans = Z * 5
-	return [cad.extrusion(trans, b).finish() for b in bases]
+@fixture
+def bases() -> List:
+	"for extrusions"
+	return make_bases()
 
 
 @fixture(params=[0, 1, 2])
-def base(request) -> Mesh:
-	yield make_bases()[request.param]
+def base(bases, request) -> Mesh:
+	"for extrusions"
+	yield bases[request.param]
 
 
 @fixture()
 def ex(base) -> Mesh:
-	trans = Z * 5
+	trans = Z * 2
 	yield cad.extrusion(trans, base).finish()
 
 
-def check_draft(drafted, angle, normal):
+@fixture
+def meshes(bases) -> List[Mesh]:
+	trans = Z * 2
+	extrudes = [cad.extrusion(trans, base).finish() for base in bases]
+	cube = cad.brick(width=3)
+	cylinder = cad.cylinder(vec3(0, 0, 0), vec3(0, 0, 3), 1).finish()
+	yield extrudes + [cube, cylinder]
+
+
+@fixture(params=range(5))
+def mesh(meshes, request) -> Mesh:
+	yield meshes[request.param]
+
+
+def check_draft(drafted, angle, normal, plot):
 	result_angles = draft_angles(drafted, normal, degrees=True)
 
 	for a in result_angles:
 		print(a)
+
+	if plot:
+		show([drafted], projection=cad.rendering.Orthographic())
 
 	angle_set = set(result_angles)
 	expected_angles = {0.0, 90.0 - angle, 90.0 + angle, 180.0}
@@ -61,27 +80,21 @@ def check_draft(drafted, angle, normal):
 
 def test_draft_extrusion(base: Mesh | Web | Wire, plot=PLOT_DEFUALT):
 	angle = 5
-	drafted = draft_extrusion(Z * 5, base, angle)
-	if plot:
-		show([drafted])
-	check_draft(drafted, angle, Z)
+	drafted = draft_extrusion(Z * 2, base, angle)
+	check_draft(drafted, angle, Z, plot)
 
 
 def test_draft_slice(ex: Mesh, plot=PLOT_DEFUALT):
 	angle = 5
 	drafted = draft_by_slice(ex, angle)
-	if plot:
-		show([drafted])
-	check_draft(drafted, angle, Z)
+	check_draft(drafted, angle, Z, plot)
 
 
-def test_draft_axis(ex: Mesh, plot=PLOT_DEFUALT):
-	axis = Axis(vec3(0, 0, 5), Z)
+def test_draft_axis(mesh: Mesh, plot=PLOT_DEFUALT):
+	axis = Axis(vec3(0, 0, 1), Z)
 	angle = 5
-	drafted = draft_by_axis(ex, axis, angle)
-	if plot:
-		show([drafted])
-	check_draft(drafted, angle, Z)
+	drafted = draft_by_axis(mesh, axis, angle)
+	check_draft(drafted, angle, Z, plot)
 
 
 def draft_angles(mesh: Mesh, n: vec3, **kwargs):
@@ -98,6 +111,12 @@ def angle_vectors(v1: vec3, v2: vec3, degrees=False) -> float:
 
 
 if __name__ == "__main__":
+
+	def make_extrusions() -> List[Tuple[Mesh, type]]:
+		bases = make_bases()
+		trans = Z * 2
+		return [cad.extrusion(trans, b).finish() for b in bases]
+
 	meshes = make_extrusions()
 	for ex in meshes:
-		test_draft_axis(ex)
+		test_draft_slice(ex)
