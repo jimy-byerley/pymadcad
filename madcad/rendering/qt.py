ff6bbe7a81f37a22e4d03f8f07437807d80065e5
bourbonut <ben.bourbon06@gmail.com>
2023-07-03 22:17:00 +0200

    Update Qt imports

--- madcad/rendering/qt.py
+++ madcad/rendering/qt.py
@@ -1,42 +1,40 @@
 import importlib
 
-# for module in ("PySide6", "PyQt6", "PyQt5"):
-for module in ("PyQt6", ):
-    # try:
-    OpenGL_suffix = ".QtOpenGLWidgets" if module == "PyQt6" else ".QtWidgets"
-    # Modules
-    QtCore    = importlib.import_module(module + ".QtCore")
-    QtWidgets = importlib.import_module(module + ".QtWidgets")
-    QtGui     = importlib.import_module(module + ".QtGui")
-    QtOpenGL = importlib.import_module(module + OpenGL_suffix)
+for module in ("PySide6", "PyQt6", "PyQt5"):
+    try:
+        OpenGL_suffix =".QtWidgets" if module == "PyQt5" else ".QtOpenGLWidgets"
+        # Modules
+        QtCore    = importlib.import_module(module + ".QtCore")
+        QtWidgets = importlib.import_module(module + ".QtWidgets")
+        QtGui     = importlib.import_module(module + ".QtGui")
+        QtOpenGL = importlib.import_module(module + OpenGL_suffix)
 
-    # Classes
-    QPoint = QtCore.QPoint
-    QPointF = QtCore.QPointF
-    QEvent = QtCore.QEvent
-    Qt     = QtCore.Qt
+        # Classes
+        QPoint = QtCore.QPoint
+        QPointF = QtCore.QPointF
+        QEvent = QtCore.QEvent
+        Qt     = QtCore.Qt
 
-    QApplication  = QtWidgets.QApplication
-    QWidget       = QtWidgets.QWidget
-    QOpenGLWidget = QtOpenGL.QOpenGLWidget
+        QApplication  = QtWidgets.QApplication
+        QWidget       = QtWidgets.QWidget
+        QOpenGLWidget = QtOpenGL.QOpenGLWidget
 
-    QSurfaceFormat = QtGui.QSurfaceFormat
-    QMouseEvent    = QtGui.QMouseEvent
-    QInputEvent    = QtGui.QInputEvent
-    QKeyEvent      = QtGui.QKeyEvent
-    QTouchEvent    = QtGui.QTouchEvent
-    QFocusEvent    = QtGui.QFocusEvent
+        QSurfaceFormat = QtGui.QSurfaceFormat
+        QMouseEvent    = QtGui.QMouseEvent
+        QInputEvent    = QtGui.QInputEvent
+        QKeyEvent      = QtGui.QKeyEvent
+        QTouchEvent    = QtGui.QTouchEvent
+        QFocusEvent    = QtGui.QFocusEvent
 
-    # Special objects
-    ApplicationAttribute = Qt.ApplicationAttribute if module == "PyQt6" else Qt
-    AA_ShareOpenGLContexts = ApplicationAttribute.AA_ShareOpenGLContexts
-    QAllEvents = (QEvent.Type if module == "PyQt6" else QEvent)
-    QMouseButton = (Qt.MouseButton if module == "PyQt6" else Qt)
-    QKeys = (Qt.Key if module == "PyQt6" else Qt)
+        # Special objects
+        ApplicationAttribute = Qt.ApplicationAttribute if module == "PyQt6" else Qt
+        AA_ShareOpenGLContexts = ApplicationAttribute.AA_ShareOpenGLContexts
+        QAllEvents = (QEvent.Type if module == "PyQt6" else QEvent)
+        QMouseButton = (Qt.MouseButton if module == "PyQt6" else Qt)
+        QKeys = (Qt.Key if module == "PyQt6" else Qt)
 
-    # Version
-    print(module)
-    QVersion = "PyQt6"
-    break
-    # except ModuleNotFoundError:
-    #     pass
+        # Version
+        QVersion = module
+        break
+    except ModuleNotFoundError:
+        pass
