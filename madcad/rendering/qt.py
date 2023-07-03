b07d8fc18cfce34c55a211332cd94f1106e3ca73
bourbonut <ben.bourbon06@gmail.com>
2023-07-03 22:22:11 +0200

    Qt imports indentation

--- madcad/rendering/qt.py
+++ madcad/rendering/qt.py
@@ -7,13 +7,13 @@ for module in ("PySide6", "PyQt6", "PyQt5"):
         QtCore    = importlib.import_module(module + ".QtCore")
         QtWidgets = importlib.import_module(module + ".QtWidgets")
         QtGui     = importlib.import_module(module + ".QtGui")
-        QtOpenGL = importlib.import_module(module + OpenGL_suffix)
+        QtOpenGL  = importlib.import_module(module + OpenGL_suffix)
 
         # Classes
-        QPoint = QtCore.QPoint
+        QPoint  = QtCore.QPoint
         QPointF = QtCore.QPointF
-        QEvent = QtCore.QEvent
-        Qt     = QtCore.Qt
+        QEvent  = QtCore.QEvent
+        Qt      = QtCore.Qt
 
         QApplication  = QtWidgets.QApplication
         QWidget       = QtWidgets.QWidget
@@ -27,11 +27,11 @@ for module in ("PySide6", "PyQt6", "PyQt5"):
         QFocusEvent    = QtGui.QFocusEvent
 
         # Special objects
-        ApplicationAttribute = Qt.ApplicationAttribute if module == "PyQt6" else Qt
+        QAllEvents             = QEvent.Type if module == "PyQt6" else QEvent
+        QMouseButton           = Qt.MouseButton if module == "PyQt6" else Qt
+        QKeys                  = Qt.Key if module == "PyQt6" else Qt
+        ApplicationAttribute   = Qt.ApplicationAttribute if module == "PyQt6" else Qt
         AA_ShareOpenGLContexts = ApplicationAttribute.AA_ShareOpenGLContexts
-        QAllEvents = (QEvent.Type if module == "PyQt6" else QEvent)
-        QMouseButton = (Qt.MouseButton if module == "PyQt6" else Qt)
-        QKeys = (Qt.Key if module == "PyQt6" else Qt)
 
         # Version
         QVersion = module
