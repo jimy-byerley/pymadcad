rewritten joint Free because of wrong hypothesis on rotation matrices

madcad/kinematic/displays.py
@@ -170,7 +170,11 @@ class ChainManip(Group):
 							for grad in jac) / self.min_colinearity)
 				# combination of the closest directions to the mouse position
 				increment = la.solve(jac @ jac.transpose() + np.eye(len(jac))*self.prec, jac @ (move * colinearity))
-				self.pose[joint] += increment.clip(-self.max_increment, self.max_increment)
+				self.pose[joint] = (np.asarray(self.pose[joint]) 
+										+ increment * min(1, self.max_increment / np.abs(increment).max())
+										) .clip(*self.chain.joints[joint].bounds)
+				if hasattr(self.chain.joints[joint], 'normalize'):
+					self.pose[joint] = self.chain.joints[joint].normalize(self.pose[joint])
 				self.parts = self.chain.parts(self.pose)
 
 	def move_translate(self, dispatcher, view, sub, evt):
@@ -210,9 +214,15 @@ class ChainManip(Group):
 					for grad in jac) / self.min_colinearity)
 				
 				increment = la.solve(jac @ jac.transpose() + np.eye(len(jac))*self.prec, jac @ (move * colinearity))
-				# increment = la.pinv(jac @ jac.transpose()) @ (jac @ (move * colinearity))
 				
-				self.pose += increment.clip(-self.max_increment, self.max_increment)
+				self.pose = structure_state((
+								flatten_state(self.pose)
+								+ increment * min(1, self.max_increment / np.abs(increment).max())
+								),
+								self.pose)
+				for i, joint in enumerate(self.chain.joints):
+					if hasattr(joint, 'normalize'):
+						self.pose[i] = joint.normalize(self.pose[i])
 				self.parts = self.chain.parts(self.pose)
 
 	def move_rotate(self, dispatcher, view, sub, evt):
@@ -257,7 +267,9 @@ class ChainManip(Group):
 				increment = la.solve(jac @ jac.transpose() + np.eye(len(jac))*self.prec, jac @ (move * colinearity))
 				# increment = la.pinv(jac @ jac.transpose()) @ (jac @ (move * colinearity))
 				
-				self.pose += increment.clip(-self.max_increment, self.max_increment)
+				self.pose = (self.pose 
+								+ increment * min(1, self.max_increment / np.abs(increment).max())
+								).clip(*self.chain.bounds)
 				self.parts = self.chain.parts(self.pose)
 
 
@@ -396,16 +408,34 @@ class KinematicManip(Group):
 				
 				increment = la.solve(jac @ jac.transpose() + np.eye(len(jac))*self.prec, jac @ (move * colinearity))
 				
-				# nprint('increment', increment, self.pose)
+				nprint('increment', increment, self.pose)
 				
-				self.pose = self.kinematic.solve(close=structure_state(
-					flatten_state(self.pose) 
-					+ increment.clip(-self.max_increment, self.max_increment),
-					self.pose))
-				# self.pose = structure_state(
+				# self.pose = self.kinematic.solve(close=structure_state(
 				# 	flatten_state(self.pose) 
-				# 	+ increment.clip(-self.max_increment, self.max_increment),
-				# 	self.pose)
+				# 	+ increment * min(1, self.max_increment / np.abs(increment).max()),
+				# 	self.pose), maxiter=10, precision=1e-3)
+				
+				self.pose = structure_state(
+					flatten_state(self.pose) 
+					+ increment * min(1, self.max_increment / np.abs(increment).max()),
+					self.pose)
+					
+				for i in range(4):
+					move = -self.kinematic.cost_residuals(self.pose)
+					jac = self.kinematic.cost_jacobian(self.pose).transpose()
+					colinearity = min(1, sum(
+						np.dot(grad, move)**2 / (normsq(grad) + normsq(move) + self.prec)
+						for grad in jac) / self.min_colinearity)
+					increment = la.solve(jac @ jac.transpose() + np.eye(len(jac))*self.prec, jac @ move)
+					self.pose = structure_state(
+						flatten_state(self.pose)
+						+ increment * min(1, self.max_increment / np.abs(increment).max()),
+						self.pose)
+						
+					for i, joint in enumerate(self.kinematic.joints):
+						if hasattr(joint, 'normalize'):
+							self.pose[i] = joint.normalize(self.pose[i])
+				
 				self.parts = self.kinematic.parts(self.pose)
 				pose = (
 					*self.pose, 
@@ -611,21 +641,28 @@ def kinematic_scheme(joints) -> '(Scheme, index)':
 	index = {}
 	centers = {}
 	for joint in joints:
-		for solid, position in zip(joint.solids, joint.position):
-			centers[solid] = centers.get(solid, 0) + vec4(position, 1)
-			index[solid] = index.get(solid, len(index))
+		if hasattr(joint, 'position'):
+			for solid, position in zip(joint.solids, joint.position):
+				centers[solid] = centers.get(solid, 0) + vec4(position, 1)
+				index[solid] = index.get(solid, len(index))
 	for solid, center in centers.items():
 		centers[solid] = center.xyz / center.w  if center.w > 1 else None
 	
+	nprint('index', index)
 	scheme = Scheme()
 	for joint in joints:
-		size = max(
-			distance(position, centers[solid])
-			for solid, position in zip(joint.solids, joint.position)
-			if centers[solid])
-		scheme += joint.scheme(index, size, centers[joint.solids[0]], centers[joint.solids[-1]])
+		if hasattr(joint, 'position'):
+			size = sum(
+				vec2(distance(position, centers[solid]), 1)
+				for solid, position in zip(joint.solids, joint.position)
+				if centers[solid])
+			size = 1.5*size.x / size.y
+			
+			sch = joint.scheme(index, size, centers[joint.solids[0]], centers[joint.solids[-1]])
+			if sch is not NotImplemented:
+				scheme += sch
 	
-	return scheme, index
+	return scheme, {v: k  for k, v in index.items()}
 		
 
 

madcad/kinematic/solver.py
@@ -156,7 +156,7 @@ def partial_difference(f, x, fx, i, d):
 	except KinematicError:
 		try:
 			p[i] = x[i] - d
-			return (f(p) - fx) / d
+			return (fx - f(p)) / d
 		except KinematicError:
 			pass
 	raise ValueError('cannot compute below or above parameter {} given value'.format(i))
@@ -207,29 +207,56 @@ class Free(Joint):
 		it is useful to control the explicit pose of a solid solid in a kinematic.
 	'''
 	bounds = (
-		np.array([-1, -1, -1,  -1, -1, -1,  -inf, -inf, -inf], float), 
-		np.array([+1, +1, +1,  +1, +1, +1,  +inf, +inf, +inf], float),
+		np.array([-2]*4 + [-inf]*3, float), 
+		np.array([+2]*4 + [+inf]*3, float), 
 		)
-	default = squeeze_homogeneous(mat4())
+	default = np.array([1,0,0,0,   0,0,0], float)
 	
 	def __init__(self, solids):
 		self.solids = solids
 	
 	def direct(self, parameters):
-		return unsqueeze_homogeneous(parameters)
+		m = mat4(quat(parameters[:4]))
+		m[3] = vec4(parameters[4:],1)
+		return m
 		
 	def inverse(self, matrix, close=None):
-		return squeeze_homogeneous(matrix)
+		return np.concatenate([quat(matrix), matrix[3].xyz])
 		
 	def grad(self, parameters):
-		h = np.zeros(squeezed_homogeneous, float)
-		grad = np.zeros((squeezed_homogeneous, 4, 4))
-		for i in range(squeezed_homogeneous):
-			h[i] = 1
-			grad[i] = unsqueeze_homogeneous(h)
-			h[i] = 0
-		grad[:,3,3] = 0
-		return grad
+		a,b,c,d,*_ = parameters
+		return (
+			mat4( 
+				 2*a,  2*d, -2*c, 0,
+				-2*d,  2*a,  2*b, 0,
+				 2*c, -2*b,  2*a, 0,
+				 0,   0,     0,   0,
+				),
+			mat4(
+				 2*b,  2*c,  2*d, 0,
+				 2*c, -2*b,  2*a, 0,
+				 2*d, -2*a, -2*b, 0,
+				 0,    0,    0,   0,
+				),
+			mat4(
+				-2*c,  2*b, -2*a, 0,
+				 2*b,  2*c,  2*d, 0,
+				 2*a,  2*d, -2*c, 0,
+				 0,    0,    0,   0,
+				),
+			mat4(
+				-2*d,  2*a,  2*b, 0,
+				-2*a, -2*d,  2*c, 0,
+				 2*b,  2*c,  2*d, 0,
+				 0,    0,    0,   0,
+				),
+			translate(X),
+			translate(Y),
+			translate(Z),
+			)
+			
+	def normalize(self, parameters):
+		return np.concatenate([normalize(quat(parameters[:4])), parameters[4:]])
 	
 	def __repr__(self):
 		return '{}({})'.format(self.__class__.__name__, self.solids)
@@ -272,9 +299,8 @@ class Reverse(Joint):
 	def __repr__(self):
 		return '{}({})'.format(self.__class__.__name__, self.joint)
 		
-	def scheme(self, scene):
-		a, b = self.joint.scheme(scene)
-		return (b, a)
+	def scheme(self, index, maxsize, attach_start, attach_end):
+		return self.joint.scheme(index, maxsize, attach_end, attach_start)
 
 class Chain(Joint):
 	''' 
@@ -558,6 +584,7 @@ class Kinematic:
 					joint = self.rev[joint]
 				chain.append(joint)
 			self.cycles.append(chain)
+		nprint(self.cycles)
 			
 	def cycles(self) -> list:
 		'''
@@ -715,19 +742,21 @@ class Kinematic:
 					lambda x: self.cost_residuals(structure_state(x, init), fixed), 
 					flatten_state(init), 
 					method = 'trf', 
-					bounds = (
-						flatten_state(mins), 
-						flatten_state(maxes),
-						), 
+					# bounds = (
+					# 	flatten_state(mins), 
+					# 	flatten_state(maxes),
+					# 	), 
 					jac = lambda x: self.cost_jacobian(structure_state(x, init), fixed), 
 					xtol = precision, 
 					max_nfev = maxiter,
+					# tr_solver = 'lsmr',
 					)
 		
-		if not res.success:
-			raise KinematicError('failed to converge: '+res.message, res)
-		if np.any(np.abs(res.fun) > precision):
-			raise KinematicError('position out of reach: no solution found for closing the kinematic cycles')
+		# if not res.success:
+		# 	raise KinematicError('failed to converge: '+res.message, res)
+		# print(res)
+		# if np.any(np.abs(res.fun) > precision):
+			# raise KinematicError('position out of reach: no solution found for closing the kinematic cycles')
 		
 		# structure results
 		result = iter(structure_state(res.x, init))
@@ -756,9 +785,9 @@ class Kinematic:
 		for joint in self.order:
 			base = poses.get(joint.solids[0]) or mat4()
 			tip = base * transforms[joint]
-			if joint.solids[-1] in poses:
-				if np.any(np.abs(poses[joint.solids[-1]] - tip) > precision):
-					raise KinematicError('position out of reach: kinematic cycles not closed')
+			# if joint.solids[-1] in poses:
+				# if np.any(np.abs(poses[joint.solids[-1]] - tip) > precision):
+					# raise KinematicError('position out of reach: kinematic cycles not closed')
 			poses[joint.solids[-1]] = tip
 		return poses
 		
@@ -787,10 +816,16 @@ class Kinematic:
 			raise KinematicError("the defined degrees of freedom cannot move") from err
 		# unsqueeze and nullify homogeneous coordinate
 		result = []
-		for grad in jac.T:
-			grad = unsqueeze_homogeneous(grad)
-			grad[3][3] = 0
-			result.append(grad)
+		# for i,grad in enumerate(jac.T):
+		# 	grad = unsqueeze_homogeneous(grad)
+		# 	grad[3][3] = 0
+		# 	result.append(grad)
+		
+		dmats = [j.grad(s)  for j,s in zip(self.joints[-len(self.outputs):], state[-len(self.outputs):])]
+		for i,djoint in enumerate(jac.T):
+			for dmat in dmats:
+				result.append(sum( dm * dj   for dm,dj in zip(dmat,djoint) ))
+					
 		return result
 		
 	def direct(self, parameters: list, close=None) -> list: