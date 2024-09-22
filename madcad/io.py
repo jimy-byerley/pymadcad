4b20f1e8b0909c5a7c07fcb7fd40d047489b6f7f
jimy-byerley <jimy.byerley@gmail.com>
2024-09-22 14:02:36 +0200

    new caching system for functions and modules

--- madcad/io.py
+++ madcad/io.py
@@ -94,7 +94,201 @@ def cachefunc(f):
 		return cache(key, lambda: f(*args, **kwargs))
 	return repl
 
+from types import ModuleType, FunctionType
+from dataclasses import dataclass
+from time import time
+import sys
+import weakref
+import inspect
+
+def cached(obj=None, name=None, file=True, recursive=True):
+	if isinstance(obj, str): 
+		name, obj = obj, None
 	
+	# directly called on a function
+	if callable(obj):
+		if name is None:
+			name = '{}{}{}.{}'.format(cachedir, os.path.sep, 
+				obj.__module__.replace('/', '_').replace('<', '').replace('>', ''), 
+				obj.__qualname__.replace('<', '').replace('>', ''),
+				)
+		
+		@wraps(obj)
+		def caching(*args, **kwargs):
+			# create a unique representation or arguments
+			code_args = inspect.signature(obj).bind(*args, **kwargs)
+			code_args.apply_defaults()
+			code_args = sorted(list(code_args.arguments.items()))
+			# the key for this cache
+			if not os.path.exists(name):
+				os.makedirs(name)
+			unique = md5(repr(code_args).encode()).hexdigest()
+			filename = '{}{}{}.pickle'.format(name, os.path.sep, unique)
+			
+			func_date = code_date(obj, recursive)
+			print('date', obj, func_date)
+			
+			# retreive from RAM cache
+			ram = caches.get(filename)
+			if ram and ram.date >= func_date:
+				result = ram.finalizer.peek()[0]
+			else:
+				# retreive from file cache
+				succeed = False
+				if file:
+					try:
+						file_date = os.path.getmtime(filename)
+						date = file_date
+						if file_date >= func_date:
+							result = pickle.load(open(filename, 'rb'))
+							succeed = True
+					except (OSError, pickle.UnpicklingError):
+						pass
+				
+				# full computation
+				if not succeed:
+					date = time()
+					result = obj(*args, **kwargs)
+				
+					# store to file cache
+					if file:
+						pickle.dump(result, open(filename, 'wb'))
+				
+				# store to RAM cache
+				record(filename, date, result)
+			
+			return result
+		return caching
+			
+	# used as a decorator
+	elif obj is None:
+		return partial(cache, name=name, target=target)
+	# badly used
+	else:
+		raise TypeError("argument must be a filename or a function")
+		
+@dataclass
+class CacheRecord:
+	date: float
+	finalizer: weakref.finalize
+	
+caches = {}
+	
+def record(key, date, data):
+	if previous := caches.pop(key, None):
+		previous.finalizer.detach()
+	caches[key] = CacheRecord(
+		date,
+		weakref.finalize(data, caches.pop, key),
+		)
+
+def code_date(func, recursive=False, memo=None) -> float:
+	''' retreive the date of the latest change made to the given code (or its dependencies) '''
+	# prevent dependency loops
+	if memo is None:
+		memo = {}
+	elif last := memo.get(id(func)):
+		return last
+	
+	# for builtin functions, we assume it is older than anything possible since they are standard and not supposed to vary
+	date = -inf
+	# retreive date from code file
+	if isinstance(func, type):
+		try: source = func.__init__
+		except AttributeError: pass
+		date = code_date(source, recursive, memo)
+	elif isinstance(func, FunctionType):
+		try: source = func.__globals__
+		except AttributeError: pass
+		date = module_date(source, recursive, memo)
+	elif isinstance(func, ModuleType):
+		try: source = func.__dict__
+		except AttributeError: pass
+		date = module_date(source, recursive, memo)
+	else:
+		return date
+	
+	return date
+	
+def module_date(obj: dict, recursive: bool, memo: dict) -> float:
+	''' retreive from a module's dictionnary '''
+	if last := memo.get(id(obj)):
+		return last
+	
+	if file := obj.get('__file__'):
+		try:
+			date = os.path.getmtime(file)
+		except OsError:
+			date = -inf
+	else:
+		date = -inf
+	
+	# store date before recursive calls
+	memo[id(obj)] = date
+	# check functions used inside
+	# imported modules are not recursed because they are supposed to be installed modules and thus not modified
+	imported = sys.modules.get(obj.get('__name__'))
+	if recursive and date != -inf and not (imported and imported.__dict__ is obj):
+		for key, dependency in obj.items():
+			date = max(date, code_date(dependency, recursive, memo))
+	
+	return date
+		
+def module(file:str=None, name:str=None, code='') -> ModuleType:
+	''' 
+		execute the given file or code as a module and return it.
+		The only difference wih `import` is that it will not be registered in `sys.modules` and therefore each time this function is called on the same file a duplicate is created.
+		
+		This function is useful if you want to import a python file which is not on the path or that you want being able to execute it multiple times
+		
+		If `name` is not specified, a default name will be deduced from the file name or from the code
+	'''
+	if file:
+		# load body and pick name from the file
+		file = os.path.realpath(file)
+		body = open(file, 'r').read()
+		if not name:
+			name = '<{}>'.format(file)
+	else:
+		# pick a default name juste like `exec` would have done
+		if not name:
+			name = '<string>'
+
+	# create a regular python module, but not registered in sys.modules
+	module = ModuleType(name)
+	module.__name__ = name
+	if file:
+		module.__file__ = file
+
+	if code:	exec(code, module.__dict__)	# first load provided code
+	if file:	exec(body, module.__dict__) # then loaded file
+	return module
+		
+def cachedmodule(file:str, name:str=None, recursive=True) -> ModuleType:
+	'''
+		This function will call `module`, then all following calls with the same file name will return the same module object until the file or one of its former dependencies has been modified.
+		
+		Warning:
+			This will not take into account eventual new dependencies added to to this module, but only look up at the dependencies of the formerly loaded and cached module
+	'''
+	if not name:
+		name = '<{}>'.format(file)
+	
+	ram = caches.get(file)
+	if ram:
+		ram_data = ram.finalizer.peek()[0]
+		file_date = code_date(ram_data, recursive)
+		if ram.date >= file_date:
+			return ram_data
+	
+	date = time()
+	data = module(file, name)
+	record(file, date, data)
+	return data
+
+
+
+
 '''
 	pickle files are the standard python serialized files, they are absolutely not secure ! so do not use it for something else than your own caching.
 '''
