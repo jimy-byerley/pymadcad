c92bafda02222bd228151dce1876c5112dbb424e
jimy byerley <jimy.byerley@gmail.com>
2025-02-08 13:20:22 +0100

    added qt importation file, this centralization will allows to select a qt version

---
+++ madcad/qt.py
@@ -0,0 +1,10 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+Signal = pyqtSignal
+
+# from PySide6.QtWidgets import *
+# from PySide6.QtGui import *
+# from PySide6.QtCore import *
+# from PySide6.QtOpenGLWidgets import *
